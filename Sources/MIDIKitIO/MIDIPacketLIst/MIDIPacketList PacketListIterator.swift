c232033cb864c32044dc3713668d16dceec2c75d
Steffan Andrews <stef@orchetect.com>
2021-04-13 12:38:19 -0700

    IO: Fixed rare MIDIPacketList.PacketListIterator crash when calling MIDIPacketNext

--- Sources/MIDIKitIO/MIDIPacketLIst/MIDIPacketList PacketListIterator.swift
+++ Sources/MIDIKitIO/MIDIPacketLIst/MIDIPacketList PacketListIterator.swift
@@ -1,6 +1,6 @@
 //
 //  MIDIPacketList PacketListIterator.swift
-//  MIDIIOKit
+//  MIDIKit
 //
 //  Created by Steffan Andrews on 2021-02-22.
 //
@@ -14,40 +14,32 @@ extension MIDIPacketList {
 		
 		public typealias Element = MIDIPacket
 		
-		// Extracted packet list info
 		var count: UInt32
-		var index: Int = -1
-		
-		// Iteration state
+		var index: Int = 0
 		var packet: MIDIPacket
 		
 		/// Initialize the packet list generator with a packet list
 		/// - parameter packetList: MIDI Packet List
-		@inline(__always) init(_ packetList: MIDIPacketList) {
+		init(_ packetList: MIDIPacketList) {
 			
 			self.packet = packetList.packet
 			self.count = packetList.numPackets
 			
 		}
 		
-		/// Provide the next element (packet)
-		@inline(__always) public mutating func next() -> Element? {
+		@_optimize(none)
+		public mutating func next() -> Element? {
 			
 			// On Intel and PowerPC, MIDIPacket is unaligned.
-			// On ARM, MIDIPacket must be 4-byte aligned; MIDIPacketNext(...) takes care of this.
+			// On ARM, MIDIPacket must be 4-byte aligned
+			// MIDIPacketNext(...) takes care of this.
 			
-			index += 1
-			
-			// ensure we are able to advance the index before proceeding,
-			// otherwise MIDIPacketNext will produce a crash (thanks, Apple)
 			guard index < count else { return nil }
 			
-			// advance to next packet if not the first packet
 			if index > 0 {
-				// advance and return pointer to next packet
-				// withUnsafePointer is needed - do not just pass packet directly into MIDIPacketNext
-				packet = withUnsafePointer(to: &packet) { MIDIPacketNext($0).pointee }
+				packet = MIDIPacketNext(&packet).pointee
 			}
+			index += 1
 			
 			return packet
 			
