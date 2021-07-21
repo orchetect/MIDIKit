815d7092dc7ad0e802a3a5e71630018b8eae2812
Steffan Andrews <stef@orchetect.com>
2021-07-20 21:48:05 -0700

    HUI: changed `.transmitFader()` `channel` param to `Int`

--- Sources/MIDIKit/HUI/HUI/Surface/Surface Transmit.swift
+++ Sources/MIDIKit/HUI/HUI/Surface/Surface Transmit.swift
@@ -50,15 +50,15 @@ extension MIDI.HUI.Surface {
     /// Transmit fader level to host
     /// - parameter level: between 0 - 16383
     public func transmitFader(level: MIDI.UInt14,
-                              channel: MIDI.UInt7) {
+                              channel: Int) {
         
         guard level.isContained(in: 0...16383) else { return }
-        guard channel.isContained(in: 0x0...0xF) else { return }
+        guard channel.isContained(in: 0x0...0x7) else { return }
         
         let msb = level.bytePair.MSB.midiUInt7
         let lsb = level.bytePair.LSB.midiUInt7
-        let channelHi = channel.asUInt8.midiUInt7
-        let channelLow = (channel.asUInt8 + 0x20).midiUInt7
+        let channelHi = channel.midiUInt7
+        let channelLow = (channel + 0x20).midiUInt7
         
         let event1 = MIDI.Event.cc(controller: channelHi, value: msb, channel: 0)
         let event2 = MIDI.Event.cc(controller: channelLow, value: lsb, channel: 0)
@@ -71,11 +71,11 @@ extension MIDI.HUI.Surface {
     /// Transmit fader touch/release message to host
     /// - parameter isTouched: `true` sends touch message, `false` sends release message
     public func transmitFader(isTouched: Bool,
-                              channel: MIDI.UInt7) {
+                              channel: Int) {
         
-        guard channel.isContained(in: 0x0...0xF) else { return }
+        guard channel.isContained(in: 0x0...0x7) else { return }
         
-        let event1 = MIDI.Event.cc(controller: 0x0F, value: channel, channel: 0)
+        let event1 = MIDI.Event.cc(controller: 0x0F, value: channel.midiUInt7, channel: 0)
         let event2 = MIDI.Event.cc(controller: 0x2F, value: isTouched ? 0x40 : 0x00, channel: 0)
         
         midiEventSendHandler?(event1)
