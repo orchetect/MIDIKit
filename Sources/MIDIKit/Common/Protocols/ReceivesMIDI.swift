b4c000a28eca49f675e55261096afe039123b308
Steffan Andrews <stef@orchetect.com>
2021-07-21 01:17:10 -0700

    Common: Added `ReceivesMIDI` protocol

---
+++ Sources/MIDIKit/Common/Protocols/ReceivesMIDI.swift
@@ -0,0 +1,26 @@
+//
+//  ReceivesMIDI.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//
+
+public protocol ReceivesMIDIEvents {
+    
+    /// Process MIDI events.
+    func midiIn(event: MIDI.Event)
+    
+    /// Process MIDI events.
+    func midiIn(events: [MIDI.Event])
+    
+}
+
+extension ReceivesMIDIEvents {
+    
+    public func midiIn(events: [MIDI.Event]) {
+        
+        for event in events {
+            midiIn(event: event)
+        }
+        
+    }
+    
+}
