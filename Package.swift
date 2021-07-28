7591043c7fcf45445b0f8bb24da68d24fcb35f37
Steffan Andrews <stef@orchetect.com>
2021-07-27 23:57:04 -0700

    Updated dependency versions

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/orchetect/SwiftRadix", from: "1.0.1")
+        .package(url: "https://github.com/orchetect/SwiftRadix", from: "1.0.3")
     ],
     
     targets: [
