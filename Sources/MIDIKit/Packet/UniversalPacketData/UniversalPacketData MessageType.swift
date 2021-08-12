4201a6f18767f0d5894172015bdb0e8c6dac0407
Steffan Andrews <stef@orchetect.com>
2021-08-12 16:14:42 -0700

    Improved `MIDI2Parser`

--- Sources/MIDIKit/Packet/UniversalPacketData/UniversalPacketData MessageType.swift
+++ Sources/MIDIKit/Packet/UniversalPacketData/UniversalPacketData MessageType.swift
@@ -18,4 +18,23 @@ extension MIDI.Packet.UniversalPacketData {
         
     }
     
+    public enum SysExStatusField: MIDI.Nibble, CaseIterable {
+        
+        /// Complete System Exclusive Message in one UMP System Exclusive.
+        case complete = 0x0
+        
+        /// System Exclusive Start UMP.
+        case start = 0x1
+        
+        /// System Exclusive Continue UMP.
+        /// There might be multiple Continue UMPs in a single message.
+        case `continue` = 0x2
+        
+        /// System Exclusive End UMP.
+        case end = 0x3
+        
+        // 0x4... are unused/reserved
+        
+    }
+    
 }
