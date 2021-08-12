5b53a2740a3e291ab28d70643cb889224a9b2fc1
Steffan Andrews <stef@orchetect.com>
2021-08-11 20:38:37 -0700

    Updated `MIDISystemInfo` project

--- Examples/MIDISystemInfo/MIDISystemInfo/AppDelegate.swift
+++ Examples/MIDISystemInfo/MIDISystemInfo/AppDelegate.swift
@@ -23,61 +23,47 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 				  defaultSubsystem: Globals.bundle.bundleID,
 				  useEmoji: .all)
 		
+        // set up midi manager
+        
+        midiManager = {
+            let newManager =
+                MIDI.IO.Manager(clientName: "MIDISystemInfo",
+                                model: "TestApp",
+                                manufacturer: "Orchetect")
+            do {
+                Log.debug("Starting MIDI manager client")
+                try newManager.start()
+            } catch {
+                Log.default(error)
+            }
+            
+            return newManager
+        }()
+        
 		// Create the window and set the content view.
 		window = NSWindow(
 		    contentRect: NSRect(x: 0, y: 0, width: 950, height: 850),
 			styleMask: [.titled, .miniaturizable, .resizable],
-		    backing: .buffered, defer: false)
-		
-		// Create the SwiftUI view that provides the window contents.
-		window.isReleasedWhenClosed = false
-		
-		window.center()
-		window.setFrameAutosaveName("Main Window")
-		
-		window.contentView = {
-			if #available(macOS 11.0, *) {
-				
-				// for Big Sur, demonstrate using @StateObject to hold the MIDI.IO.Manager in a subordinate scope and not in AppDelegate
-				window.title = "MIDIKit Test Harness (Big Sur)"
-				return NSHostingView(
-					rootView: ContentViewBigSur()
-						.environment(\.hostingWindow, { [weak window] in window })
-				)
-				
-			} else {
-				
-				// for Catalina, since @StateObject is not available for use, demonstrate storing the MIDI.IO.Manager instance in AppDelegate and passing it by reference into ContentView's weak @ObservedObject storage.
-				midiManager = {
-					let newManager =
-						MIDI.IO.Manager(clientName: "MIDISystemInfo",
-										model: "TestApp",
-										manufacturer: "Orchetect")
-					do {
-						Log.debug("Starting MIDI manager client")
-						try newManager.start()
-					} catch {
-						Log.default(error)
-					}
-					
-					return newManager
-				}()
-				
-				window.title = "MIDIKit Test Harness (Catalina)"
-				return NSHostingView(
-					rootView: ContentViewCatalina(midiManager: midiManager!)
-						.environment(\.hostingWindow, { [weak window] in window })
-				)
-				
-			}
-		}()
-		
-		window.makeKeyAndOrderFront(nil)
-		
-	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
+            backing: .buffered, defer: false)
+        
+        // Create the SwiftUI view that provides the window contents.
+        window.isReleasedWhenClosed = false
+        
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        
+        window.title = "MIDIKit Test Harness (Catalina)"
+        window.contentView = NSHostingView(
+            rootView: ContentViewCatalina(midiManager: midiManager!)
+                .environment(\.hostingWindow, { [weak window] in window })
+        )
+        
+        window.makeKeyAndOrderFront(nil)
+        
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
 
 }
