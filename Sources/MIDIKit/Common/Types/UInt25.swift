3d267e4ba9e5bec390e620f9762c4f0b0a970cd9
Steffan Andrews <stef@orchetect.com>
2021-10-04 23:31:09 -0700

    Added `MIDI.UInt25`

---
+++ Sources/MIDIKit/Common/Types/UInt25.swift
@@ -0,0 +1,141 @@
+//
+//  UInt25.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//
+
+extension MIDI {
+    
+    /// A 25-bit unsigned integer value type used in `MIDIKit`.
+    public struct UInt25: MIDIKitIntegerProtocol {
+        
+        // MARK: Storage
+        
+        public typealias Storage = UInt32
+        public internal(set) var value: Storage
+        
+        // MARK: Inits
+        
+        public init() {
+            value = 0
+        }
+        
+        public init<T: BinaryInteger>(_ source: T) {
+            if source < Self.min(Storage.self) {
+                Exception.underflow.raise(reason: "UInt25 integer underflowed")
+            }
+            if source > Self.max(Storage.self) {
+                Exception.overflow.raise(reason: "UInt25 integer overflowed")
+            }
+            value = Storage(source)
+        }
+        
+        public init<T: BinaryFloatingPoint>(_ source: T) {
+            if source < Self.min(T.self) {
+                Exception.underflow.raise(reason: "UInt25 integer underflowed")
+            }
+            if UInt32(source) > Self.max(UInt32.self) {
+                Exception.overflow.raise(reason: "UInt25 integer overflowed")
+            }
+            value = Storage(source)
+        }
+        
+        // MARK: Constants
+        
+        public static let bitWidth: Int = 25
+        
+        public static func min<T: BinaryInteger>(_ ofType: T.Type) -> T { 0 }
+        public static func min<T: BinaryFloatingPoint>(_ ofType: T.Type) -> T { 0 }
+        
+        // 0b1_00000000_00000000_00000000
+        /// Neutral midpoint
+        public static let midpoint = Self(Self.midpoint(Storage.self))
+        public static func midpoint<T: BinaryInteger>(_ ofType: T.Type) -> T { 16777216 }
+        
+        // 0b1_11111111_11111111_11111111
+        public static func max<T: BinaryInteger>(_ ofType: T.Type) -> T { 33554431 }
+        public static func max<T: BinaryFloatingPoint>(_ ofType: T.Type) -> T { 33554431 }
+        
+        // MARK: Computed properties
+        
+        /// Returns the integer as a `UInt32` instance
+        public var uInt32Value: UInt32 { value }
+        
+    }
+    
+}
+
+extension MIDI.UInt25: ExpressibleByIntegerLiteral {
+    
+    public typealias IntegerLiteralType = Storage
+    
+    public init(integerLiteral value: Storage) {
+        self.init(value)
+    }
+    
+}
+
+extension MIDI.UInt25: Equatable, Comparable {
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.value == rhs.value
+    }
+    
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        lhs.value < rhs.value
+    }
+    
+}
+
+extension MIDI.UInt25: Hashable {
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(value)
+    }
+    
+}
+
+extension MIDI.UInt25: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case value = "UInt25"
+    }
+    
+}
+
+extension MIDI.UInt25: CustomStringConvertible {
+    
+    public var description: String {
+        "\(value)"
+    }
+    
+}
+
+// MARK: - Standard library extensions
+
+extension BinaryInteger {
+    
+    /// Convenience initializer for `MIDI.UInt25`.
+    public var toMIDIUInt25: MIDI.UInt25 {
+        MIDI.UInt25(self)
+    }
+    
+    /// Convenience initializer for `MIDI.UInt25(exactly:)`.
+    public var toMIDIUInt25Exactly: MIDI.UInt25? {
+        MIDI.UInt25(exactly: self)
+    }
+    
+}
+
+extension BinaryFloatingPoint {
+    
+    /// Convenience initializer for `MIDI.UInt25`.
+    public var toMIDIUInt25: MIDI.UInt25 {
+        MIDI.UInt25(self)
+    }
+    
+    /// Convenience initializer for `MIDI.UInt25(exactly:)`.
+    public var toMIDIUInt25Exactly: MIDI.UInt25? {
+        MIDI.UInt25(exactly: self)
+    }
+    
+}
---
+++ Tests/MIDIKitTests/Common/Types/UInt25 Tests.swift
@@ -0,0 +1,282 @@
+//
+//  UInt25 Tests.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//
+
+#if !os(watchOS)
+
+import XCTest
+@testable import MIDIKit
+import XCTestExtensions
+
+final class UInt25_Tests: XCTestCase {
+	
+	fileprivate let _max = 0b1_11111111_11111111_11111111
+	
+	func testInit_BinaryInteger() {
+		
+		// default
+		
+		XCTAssertEqual(MIDI.UInt25().intValue, 0)
+		
+		// different integer types
+		
+		XCTAssertEqual(MIDI.UInt25(0).intValue, 0)
+		XCTAssertEqual(MIDI.UInt25(UInt8(0)).intValue, 0)
+		XCTAssertEqual(MIDI.UInt25(UInt16(0)).intValue, 0)
+		
+		// values
+		
+		XCTAssertEqual(MIDI.UInt25(1).intValue, 1)
+		XCTAssertEqual(MIDI.UInt25(2).intValue, 2)
+		
+		// overflow
+		
+        _XCTAssertThrows {
+			_ = MIDI.UInt25(0 - 1)
+		}
+		
+        _XCTAssertThrows { [self] in
+			_ = MIDI.UInt25(_max + 1)
+		}
+		
+	}
+	
+	func testInit_BinaryInteger_Exactly() {
+		
+		// typical
+		
+		XCTAssertEqual(MIDI.UInt25(exactly: 0)?.intValue, 0)
+		
+		XCTAssertEqual(MIDI.UInt25(exactly: 1)?.intValue, 1)
+		
+		XCTAssertEqual(MIDI.UInt25(exactly: _max)?.intValue, _max)
+		
+		// overflow
+		
+		XCTAssertNil(MIDI.UInt25(exactly: -1))
+		
+		XCTAssertNil(MIDI.UInt25(exactly: _max + 1))
+		
+	}
+    
+	func testInit_BinaryInteger_Clamping() {
+		
+		// within range
+		
+		XCTAssertEqual(MIDI.UInt25(clamping: 0).intValue, 0)
+		XCTAssertEqual(MIDI.UInt25(clamping: 1).intValue, 1)
+		XCTAssertEqual(MIDI.UInt25(clamping: _max).intValue, _max)
+		
+		// overflow
+		
+		XCTAssertEqual(MIDI.UInt25(clamping: -1).intValue, 0)
+		XCTAssertEqual(MIDI.UInt25(clamping: _max + 1).intValue, _max)
+		
+	}
+    
+    func testInit_BinaryFloatingPoint() {
+        
+        XCTAssertEqual(MIDI.UInt25(Double(0)).intValue, 0)
+        XCTAssertEqual(MIDI.UInt25(Double(1)).intValue, 1)
+        XCTAssertEqual(MIDI.UInt25(Double(5.9)).intValue, 5)
+        
+        XCTAssertEqual(MIDI.UInt25(Float(0)).intValue, 0)
+        XCTAssertEqual(MIDI.UInt25(Float(1)).intValue, 1)
+        XCTAssertEqual(MIDI.UInt25(Float(5.9)).intValue, 5)
+        
+        // overflow
+        
+        _XCTAssertThrows {
+            _ = MIDI.UInt25(Double(0 - 1))
+            _ = MIDI.UInt25(Float(0 - 1))
+        }
+        
+        _XCTAssertThrows { [self] in
+            _ = MIDI.UInt25(Double(_max + 1))
+            _ = MIDI.UInt25(Float(_max + 1))
+        }
+        
+    }
+    
+    func testInit_BinaryFloatingPoint_Exactly() {
+        
+        // typical
+        
+        XCTAssertEqual(MIDI.UInt25(exactly: 0.0), 0)
+        
+        XCTAssertEqual(MIDI.UInt25(exactly: 1.0), 1)
+        
+        XCTAssertEqual(MIDI.UInt25(exactly: Double(_max))?.intValue, _max)
+        
+        // overflow
+        
+        XCTAssertNil(MIDI.UInt25(exactly: -1.0))
+        
+        XCTAssertNil(MIDI.UInt25(exactly: Double(_max) + 1.0))
+        
+    }
+	
+	func testMin() {
+		
+		XCTAssertEqual(MIDI.UInt25.min.intValue, 0)
+		
+	}
+	
+	func testMax() {
+		
+		XCTAssertEqual(MIDI.UInt25.max.intValue, _max)
+		
+	}
+	
+	func testComputedProperties() {
+		
+		XCTAssertEqual(MIDI.UInt25(1).intValue, 1)
+		XCTAssertEqual(MIDI.UInt25(1).uInt32Value, 1)
+		
+	}
+	
+	func testEquatable() {
+		
+		XCTAssertTrue(MIDI.UInt25(0) == MIDI.UInt25(0))
+		XCTAssertTrue(MIDI.UInt25(1) == MIDI.UInt25(1))
+		XCTAssertTrue(MIDI.UInt25(_max) == MIDI.UInt25(_max))
+		
+		XCTAssertTrue(MIDI.UInt25(0) != MIDI.UInt25(1))
+		
+	}
+	
+	func testComparable() {
+		
+		XCTAssertFalse(MIDI.UInt25(0) > MIDI.UInt25(0))
+		XCTAssertFalse(MIDI.UInt25(1) > MIDI.UInt25(1))
+		XCTAssertFalse(MIDI.UInt25(_max) > MIDI.UInt25(_max))
+		
+		XCTAssertTrue(MIDI.UInt25(0) < MIDI.UInt25(1))
+		XCTAssertTrue(MIDI.UInt25(1) > MIDI.UInt25(0))
+		
+	}
+	
+	func testHashable() {
+		
+		let set = Set<MIDI.UInt25>([0, 1, 1, 2])
+		
+		XCTAssertEqual(set.count, 3)
+		XCTAssertTrue(set == [0,1,2])
+		
+	}
+	
+	func testCodable() {
+		
+		let encoder = JSONEncoder()
+		let decoder = JSONDecoder()
+		
+		// encode
+		
+		let encoded = try? encoder.encode(MIDI.UInt25(_max))
+		
+		let encodedString = String(data: encoded!, encoding: .utf8)
+		
+		XCTAssertEqual(encodedString, #"{"UInt25":33554431}"#)
+		
+		// decode
+		
+		let decoded = try? decoder.decode(MIDI.UInt25.self, from: encoded!)
+		
+		// assert value is identical to source
+		
+		XCTAssertEqual(decoded, MIDI.UInt25(_max))
+		
+	}
+	
+	// MARK: - Standard library extensions
+	
+	func testBinaryInteger_UInt25() {
+		
+		XCTAssertEqual(10.toMIDIUInt25, 10)
+		
+		XCTAssertEqual(Int8(10).toMIDIUInt25, 10)
+		XCTAssertEqual(UInt8(10).toMIDIUInt25, 10)
+		
+		XCTAssertEqual(Int16(10).toMIDIUInt25, 10)
+		XCTAssertEqual(UInt16(10).toMIDIUInt25, 10)
+		
+	}
+	
+    func testBinaryInteger_UInt25Exactly() {
+        
+        XCTAssertEqual(0b0_00000000_00000000_00000000.toMIDIUInt25Exactly,
+                       0b0_00000000_00000000_00000000)
+        XCTAssertEqual(0b1_11111111_11111111_11111111.toMIDIUInt25Exactly,
+                       0b1_11111111_11111111_11111111)
+        
+        XCTAssertEqual(Int8(10).toMIDIUInt25Exactly, 10)
+        XCTAssertEqual(UInt8(10).toMIDIUInt25Exactly, 10)
+        
+        XCTAssertEqual(Int16(10).toMIDIUInt25Exactly, 10)
+        XCTAssertEqual(UInt16(10).toMIDIUInt25Exactly, 10)
+        
+        // nil (overflow)
+        
+        XCTAssertNil(0b10_00000000_00000000_00000000.toMIDIUInt25Exactly)
+        
+    }
+    
+    func testBinaryInteger_Init_UInt25() {
+        
+        XCTAssertEqual(Int(10.toMIDIUInt25), 10)
+        XCTAssertEqual(Int(exactly: 10.toMIDIUInt25), 10)
+        
+        XCTAssertEqual(Int(exactly: 0b1_11111111_11111111_11111111.toMIDIUInt25),
+                       0b1_11111111_11111111_11111111)
+        XCTAssertNil(UInt8(exactly: 0b1_1111_1111.toMIDIUInt25))
+        
+    }
+    
+    // MARK: - Operators
+    
+    func testOperators() {
+        
+        XCTAssertEqual(1.toMIDIUInt25 + 1              , 2.toMIDIUInt25)
+        XCTAssertEqual(1 + 1.toMIDIUInt25              , 2.toMIDIUInt25)
+        XCTAssertEqual(1.toMIDIUInt25 + 1.toMIDIUInt25 , 2)
+        
+        XCTAssertEqual(2.toMIDIUInt25 - 1              , 1.toMIDIUInt25)
+        XCTAssertEqual(2 - 1.toMIDIUInt25              , 1.toMIDIUInt25)
+        XCTAssertEqual(2.toMIDIUInt25 - 1.toMIDIUInt25 , 1)
+        
+        XCTAssertEqual(2.toMIDIUInt25 * 2              , 4.toMIDIUInt25)
+        XCTAssertEqual(2 * 2.toMIDIUInt25              , 4.toMIDIUInt25)
+        XCTAssertEqual(2.toMIDIUInt25 * 2.toMIDIUInt25 , 4)
+        
+        XCTAssertEqual(8.toMIDIUInt25 / 2              , 4.toMIDIUInt25)
+        XCTAssertEqual(8 / 2.toMIDIUInt25              , 4.toMIDIUInt25)
+        XCTAssertEqual(8.toMIDIUInt25 / 2.toMIDIUInt25 , 4)
+        
+        XCTAssertEqual(8.toMIDIUInt25 % 3              , 2.toMIDIUInt25)
+        XCTAssertEqual(8 % 3.toMIDIUInt25              , 2.toMIDIUInt25)
+        XCTAssertEqual(8.toMIDIUInt25 % 3.toMIDIUInt25 , 2)
+        
+    }
+    
+    func testAssignmentOperators() {
+        
+        var val = MIDI.UInt25(2)
+        
+        val += 5
+        XCTAssertEqual(val, 7)
+        
+        val -= 5
+        XCTAssertEqual(val, 2)
+        
+        val *= 3
+        XCTAssertEqual(val, 6)
+        
+        val /= 3
+        XCTAssertEqual(val, 2)
+        
+    }
+    
+}
+
+#endif
