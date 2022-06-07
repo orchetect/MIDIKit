80a88e1a7b1d95f5bca82c1aaf3b294063291c28
Steffan Andrews <stef@orchetect.com>
2022-06-07 00:27:17 -0700

    Added iOS UIKit example: VirtualInput

---
+++ Examples/iOS UIKit/VirtualInput/VirtualInput/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  VirtualInput
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//
+
+import UIKit
+import MIDIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    let midiManager = MIDI.IO.Manager(clientName: "TestAppMIDIManager",
+                                      model: "TestApp",
+                                      manufacturer: "MyCompany")
+    
+    let virtualInputName = "TestApp Input"
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        do {
+            print("Starting MIDI services.")
+            try midiManager.start()
+        } catch {
+            print("Error starting MIDI services:", error.localizedDescription)
+        }
+        
+        do {
+            print("Creating virtual MIDI input.")
+            try midiManager.addInput(
+                name: virtualInputName,
+                tag: virtualInputName,
+                uniqueID: .userDefaultsManaged(key: virtualInputName),
+                receiveHandler: .eventsLogging(filterActiveSensingAndClock: true)
+            )
+        } catch {
+            print("Error creating virtual MIDI input:", error.localizedDescription)
+        }
+        
+        return true
+        
+    }
+    
+}
