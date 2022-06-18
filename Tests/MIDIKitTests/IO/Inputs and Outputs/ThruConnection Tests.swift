a44d75aad4595f4a1f5fd221d3e964a160af00fe
Steffan Andrews <stef@orchetect.com>
2022-06-17 20:49:26 -0700

    Expanded ThruConnection Tests

--- Tests/MIDIKitTests/IO/Inputs and Outputs/ThruConnection Tests.swift
+++ Tests/MIDIKitTests/IO/Inputs and Outputs/ThruConnection Tests.swift
@@ -7,44 +7,259 @@
 #if shouldTestCurrentPlatform && !targetEnvironment(simulator)
 
 import XCTest
-import MIDIKit
+@testable import MIDIKit
 import CoreMIDI
 
 final class InputsAndOutputs_ThruConnection_Tests: XCTestCase {
-	
-	func testThruConnection() throws {
-
+    
+    override func setUp() {
+        wait(sec: 0.2)
+    }
+    
+    @MIDI.Atomic private var connEvents: [MIDI.Event] = []
+    
+    func testNonPersistentThruConnection() throws {
+        
         let manager = MIDI.IO.Manager(clientName: UUID().uuidString,
                                       model: "MIDIKit123",
                                       manufacturer: "MIDIKit")
         
-		// start midi client
-		try manager.start()
-		wait(sec: 0.1)
+        // start midi client
+        try manager.start()
+        wait(sec: 0.1)
+        
+        // initial state
+        connEvents.removeAll()
+        
+        // create virtual input
+        let input1Tag = "input1"
+        try manager.addInput(
+            name: "MIDIKit IO Tests thruConnection In",
+            tag: input1Tag,
+            uniqueID: .none, // allow system to generate random ID
+            receiveHandler: .events(translateMIDI1NoteOnZeroVelocityToNoteOff: false) { events in
+                self.connEvents.append(contentsOf: events)
+            })
+        let input1 = try XCTUnwrap(manager.managedInputs[input1Tag])
+        //let input1ID = try XCTUnwrap(input1.uniqueID)
+        //let input1Ref = try XCTUnwrap(input1.coreMIDIInputPortRef)
+        
+        // create a virtual output
+        let output1Tag = "output1"
+        try manager.addOutput(
+            name: "MIDIKit IO Tests thruConnection Out",
+            tag: output1Tag,
+            uniqueID: .none // allow system to generate random ID
+        )
+        let output1 = try XCTUnwrap(manager.managedOutputs[output1Tag])
+        //let output1ID = try XCTUnwrap(output1.uniqueID)
+        //let output1Ref = try XCTUnwrap(output1.coreMIDIOutputPortRef)
+        
+        wait(sec: 0.2)
+        
+        // add new connection
+        
+        let connTag = "testThruConnection"
+        try manager.addThruConnection(
+            outputs: [output1.endpoint],
+            inputs: [input1.endpoint],
+            tag: connTag,
+            lifecycle: .nonPersistent
+        )
+        
+        XCTAssertNotNil(manager.managedThruConnections[connTag])
+        XCTAssert(manager.managedThruConnections[connTag]?.coreMIDIThruConnectionRef != .init())
+        
+        wait(sec: 0.2)
+        
+        // send an event - it should be received by the input
+        try output1.send(event: .start())
+        wait(for: connEvents, equals: [.start()], timeout: 1.0)
+        connEvents = []
 
-		// add new connection
+        manager.remove(.nonPersistentThruConnection, .withTag(connTag))
+        
+        XCTAssertNil(manager.managedThruConnections[connTag])
+        
+        // send an event - it should not be received by the input
+        try output1.send(event: .start())
+        wait(sec: 0.2) // wait a bit in case an event is sent
+        XCTAssertEqual(connEvents, [])
+        connEvents = []
+        
+        // Due to a Swift Core MIDI bug, all thru connections are created as persistent.
+        // - See for details: https://openradar.appspot.com/radar?id=5043482339049472
+        // - So having passed .nonPersistent had the effect of creating a persistent
+        //   connection with an empty ownerID.
+        let conns = try manager.unmanagedPersistentThruConnections(ownerID: "")
 
-		let tag1 = "1"
+        XCTAssert(conns.isEmpty)
+        
+        // as a failsafe, clean up any persistent connections with an empty owner ID
+        manager.removeAllUnmanagedPersistentThruConnections(ownerID: "")
+        
+    }
+    
+    func testPersistentThruConnection() throws {
+        
+        let manager = MIDI.IO.Manager(clientName: UUID().uuidString,
+                                      model: "MIDIKit123",
+                                      manufacturer: "MIDIKit")
+        
+        // start midi client
+        try manager.start()
+        wait(sec: 0.1)
+        
+        // initial state
+        connEvents.removeAll()
+        let ownerID = "com.orchetect.midikit.testNonPersistentThruConnection"
+        manager.removeAllUnmanagedPersistentThruConnections(ownerID: ownerID)
+        
+//        let input1 = try XCTUnwrap(
+//            manager.endpoints.inputsUnowned
+//                .first(withDisplayName: "IAC Driver Bus 1")
+//        )
+//
+//        let output1 = try XCTUnwrap(
+//            manager.endpoints.outputsUnowned
+//                .first(withDisplayName: "CP33 Port1")
+//        )
+        
+        // create virtual input
+        let input1Tag = "input1"
+        try manager.addInput(
+            name: "MIDIKit IO Tests thruConnection In",
+            tag: input1Tag,
+            uniqueID: .none, // allow system to generate random ID
+            receiveHandler: .events(translateMIDI1NoteOnZeroVelocityToNoteOff: false) { events in
+                self.connEvents.append(contentsOf: events)
+            })
+        let input1 = try XCTUnwrap(manager.managedInputs[input1Tag])
+        //let input1ID = try XCTUnwrap(input1.uniqueID)
+        //let input1Ref = try XCTUnwrap(input1.coreMIDIInputPortRef)
+        
+        wait(sec: 0.2)
+        
+        // create a virtual output
+        let output1Tag = "output1"
+        try manager.addOutput(
+            name: "MIDIKit IO Tests thruConnection Out",
+            tag: output1Tag,
+            uniqueID: .none // allow system to generate random ID
+        )
+        let output1 = try XCTUnwrap(manager.managedOutputs[output1Tag])
+        //let output1ID = try XCTUnwrap(output1.uniqueID)
+        //let output1Ref = try XCTUnwrap(output1.coreMIDIOutputPortRef)
+        
+        wait(sec: 0.2)
+        
+        // add new connection
+        
+        let connTag = "testThruConnection"
+        try manager.addThruConnection(
+            outputs: [output1.endpoint],
+            inputs: [input1.endpoint],
+            tag: connTag,
+            lifecycle: .persistent(ownerID: ownerID)
+        )
+        
+        wait(sec: 0.2)
 
-		do {
-			try manager.addThruConnection(
-				outputs: [],
-				inputs: [],
-				tag: tag1,
-                lifecycle: .nonPersistent,
-                params: .init()
-			)
-		} catch let err as MIDI.IO.MIDIError {
-			XCTFail("\(err)") ; return
-		} catch {
-			XCTFail(error.localizedDescription) ; return
-		}
+        XCTAssertEqual(try manager.unmanagedPersistentThruConnections(ownerID: ownerID).count, 1)
 
-		XCTAssertNotNil(manager.managedThruConnections[tag1])
-		
-	}
+        // send an event - it should be received by the input
+        try output1.send(event: .start())
+        wait(for: connEvents, equals: [.start()], timeout: 1.0)
+        connEvents = []
 
+        manager.removeAllUnmanagedPersistentThruConnections(ownerID: ownerID)
+
+        XCTAssertEqual(try manager.unmanagedPersistentThruConnections(ownerID: ownerID).count, 0)
+        
+        XCTAssertEqual(manager.managedThruConnections.count, 0)
+        
+        // send an event - it should not be received by the input
+        try output1.send(event: .start())
+        wait(sec: 0.2) // wait a bit in case an event is sent
+        XCTAssertEqual(connEvents, [])
+        connEvents = []
+        
+    }
+    
+    /// Tests getting thru connection parameters from Core MIDI after creating the thru connection and verifying they are correct.
+    func testGetParams() throws {
+        
+        let manager = MIDI.IO.Manager(clientName: UUID().uuidString,
+                                      model: "MIDIKit123",
+                                      manufacturer: "MIDIKit")
+        
+        // start midi client
+        try manager.start()
+        wait(sec: 0.1)
+        
+        // initial state
+        connEvents.removeAll()
+        
+        // create virtual input
+        let input1Tag = "input1"
+        try manager.addInput(
+            name: "MIDIKit IO Tests thruConnection In",
+            tag: input1Tag,
+            uniqueID: .none, // allow system to generate random ID
+            receiveHandler: .events(translateMIDI1NoteOnZeroVelocityToNoteOff: false) { events in
+                self.connEvents.append(contentsOf: events)
+            })
+        let input1 = try XCTUnwrap(manager.managedInputs[input1Tag])
+        let input1ID = try XCTUnwrap(input1.uniqueID)
+        let input1Ref = try XCTUnwrap(input1.coreMIDIInputPortRef)
+        
+        // create a virtual output
+        let output1Tag = "output1"
+        try manager.addOutput(
+            name: "MIDIKit IO Tests thruConnection Out",
+            tag: output1Tag,
+            uniqueID: .none // allow system to generate random ID
+        )
+        let output1 = try XCTUnwrap(manager.managedOutputs[output1Tag])
+        let output1ID = try XCTUnwrap(output1.uniqueID)
+        let output1Ref = try XCTUnwrap(output1.coreMIDIOutputPortRef)
+        
+        wait(sec: 0.2)
+        
+        // add new connection
+        
+        let connTag = "testThruConnection"
+        try manager.addThruConnection(
+            outputs: [output1.endpoint],
+            inputs: [input1.endpoint],
+            tag: connTag,
+            lifecycle: .nonPersistent
+        )
+        
+        XCTAssertNotNil(manager.managedThruConnections[connTag])
+        XCTAssert(manager.managedThruConnections[connTag]?.coreMIDIThruConnectionRef != .init())
+        
+        wait(sec: 0.2)
+        
+        let connRef = manager.managedThruConnections[connTag]!.coreMIDIThruConnectionRef!
+        let getParams = try XCTUnwrap(try MIDI.IO.getThruConnectionParameters(ref: connRef))
+        
+        XCTAssertEqual(getParams.numSources, 1)
+        XCTAssertEqual(getParams.sources.0.endpointRef, output1Ref)
+        XCTAssertEqual(getParams.sources.0.uniqueID, output1ID.coreMIDIUniqueID)
+        XCTAssertEqual(getParams.numDestinations, 1)
+        XCTAssertEqual(getParams.destinations.0.endpointRef, input1Ref)
+        XCTAssertEqual(getParams.destinations.0.uniqueID, input1ID.coreMIDIUniqueID)
+        
+        manager.remove(.nonPersistentThruConnection, .withTag(connTag))
+        
+        XCTAssertNil(manager.managedThruConnections[connTag])
+        
+        // as a failsafe, clean up any persistent connections with an empty owner ID
+        manager.removeAllUnmanagedPersistentThruConnections(ownerID: "")
+        
+    }
+    
 }
 
 #endif
-
