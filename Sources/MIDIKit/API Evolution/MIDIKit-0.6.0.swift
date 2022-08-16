9614a820b19a322e2ead3ade2b4be544ffedf810
Steffan Andrews <stef@orchetect.com>
2022-08-15 18:01:03 -0700

    Updated 0.6.0 API evolution

--- Sources/MIDIKit/API Evolution/MIDIKit-0.6.0.swift
+++ Sources/MIDIKit/API Evolution/MIDIKit-0.6.0.swift
@@ -385,3 +385,46 @@ extension MIDIIdentifierPersistence {
         storeHandler: (MIDI.IO.UniqueID?) -> Void
     ) -> Self { fatalError() }
 }
+
+// MARK: Category Extensions for Type conversion
+
+extension BinaryInteger {
+    @available(*, unavailable, renamed: "toUInt4")
+    public var toMIDIUInt4: UInt4 { fatalError() }
+    
+    @available(*, unavailable, renamed: "toUInt4Exactly")
+    public var toMIDIUInt4Exactly: UInt4? { fatalError() }
+}
+
+extension BinaryInteger {
+    @available(*, unavailable, renamed: "toUInt7")
+    public var toMIDIUInt7: UInt7 { fatalError() }
+    
+    @available(*, unavailable, renamed: "toUInt7Exactly")
+    public var toMIDIUInt7Exactly: UInt7? { fatalError() }
+}
+
+extension BinaryInteger {
+    @available(*, unavailable, renamed: "toUInt9")
+    public var toMIDIUInt9: UInt9 { fatalError() }
+    
+    @available(*, unavailable, renamed: "toUInt9Exactly")
+    public var toMIDIUInt9Exactly: UInt9? { fatalError() }
+}
+
+extension BinaryInteger {
+    @available(*, unavailable, renamed: "toUInt14")
+    public var toMIDIUInt14: UInt14 { fatalError() }
+    
+    @available(*, unavailable, renamed: "toUInt14Exactly")
+    public var toMIDIUInt14Exactly: UInt14? { fatalError() }
+}
+
+extension BinaryInteger {
+    @available(*, unavailable, renamed: "toUInt25")
+    public var toMIDIUInt25: UInt25 { fatalError() }
+    
+    @available(*, unavailable, renamed: "toUInt25Exactly")
+    public var toMIDIUInt25Exactly: UInt25? { fatalError() }
+}
+
