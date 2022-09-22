c5ca2f381b03209f69a3999e317c50ad07f13871
Steffan Andrews <stef@orchetect.com>
2022-09-22 15:49:15 -0700

    Updated inline docs

--- Sources/MIDIKitControlSurfaces/HUI/HUIEvent/HUIEvent ChannelStripComponent.swift
+++ Sources/MIDIKitControlSurfaces/HUI/HUIEvent/HUIEvent ChannelStripComponent.swift
@@ -9,43 +9,40 @@ import MIDIKitCore
 extension HUIEvent {
     /// A discrete component of a HUI channel strip and its state change.
     public enum ChannelStripComponent: Equatable, Hashable {
-        #warning("> finish inline docs")
-        
+        /// Stereo Level Meter.
         case levelMeter(side: HUIModel.StereoLevelMeter.Side, level: Int)
         
-        
+        /// Record Ready Button LED.
         case recordReady(state: Bool)
         
-        
+        /// Insert Button LED.
         case insert(state: Bool)
         
-        
+        /// V-Sel Button LED.
         case vPotSelect(state: Bool)
         
         /// V-Pot encoding.
         case vPot(value: HUIVPotValue)
         
-        
+        /// Auto(mation) Button LED.
         case auto(state: Bool)
         
-        
+        /// Solo Button LED.
         case solo(state: Bool)
         
-        
+        /// Mute Button LED.
         case mute(state: Bool)
         
+        /// 4-character channel name text display.
+        case nameDisplay(text: HUISmallDisplayString)
         
-        case nameTextDisplay(text: HUISmallDisplayString)
-        
-        
+        /// Select Button LED.
         case select(state: Bool)
         
-        
+        /// Motorized Fader touched state.
         case faderTouched(state: Bool)
         
-        
+        /// Motorized Fader level.
         case faderLevel(level: UInt14)
-        
-        
     }
 }
