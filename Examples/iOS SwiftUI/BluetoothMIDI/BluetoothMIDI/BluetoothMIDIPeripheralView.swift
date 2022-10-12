03b17b59e67f172b4d1b2175a80590b6398e5b40
Steffan Andrews <stef@orchetect.com>
2022-10-11 22:07:58 -0700

    Added local Bluetooth peripheral support to Bluetooth example projects

---
+++ Examples/iOS SwiftUI/BluetoothMIDI/BluetoothMIDI/BluetoothMIDIPeripheralView.swift
@@ -0,0 +1,45 @@
+//
+//  BluetoothMIDIPeripheralView.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(iOS)
+
+import UIKit
+import SwiftUI
+import CoreAudioKit
+
+struct BluetoothMIDIPeripheralView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> BTMIDIPeripheralViewController {
+        BTMIDIPeripheralViewController()
+    }
+    
+    func updateUIViewController(
+        _ uiViewController: BTMIDIPeripheralViewController,
+        context: Context
+    ) { }
+    
+    typealias UIViewControllerType = BTMIDIPeripheralViewController
+}
+
+class BTMIDIPeripheralViewController: CABTMIDILocalPeripheralViewController {
+    var uiViewController: UIViewController?
+    
+    override public func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(doneAction)
+        )
+    }
+    
+    @objc
+    public func doneAction() {
+        uiViewController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+#endif
