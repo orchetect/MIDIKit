03870c32c7cfb3b6eb5da0c098dbdf2f3dbb2b83
Steffan Andrews <stef@orchetect.com>
2022-12-14 22:26:50 -0800

    Bumped TimecodeKit dependency to min 1.6.0

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/orchetect/TimecodeKit", branch: "fcpxml-framerate"),
+        .package(url: "https://github.com/orchetect/TimecodeKit", from: "1.6.0"),
         
         // testing-only:
         .package(url: "https://github.com/orchetect/XCTestUtils", from: "1.0.1")
