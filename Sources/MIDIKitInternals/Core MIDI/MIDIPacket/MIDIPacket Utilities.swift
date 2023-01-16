d1a8843910ac73d64592eba4f6e21a3f06e547a4
Steffan Andrews <stef@orchetect.com>
2023-01-15 23:08:00 -0800

    Added `rawBytes` to `UnsafeMutablePointer<MIDIPacket>`

--- Sources/MIDIKitInternals/Core MIDI/MIDIPacket/MIDIPacket Utilities.swift
+++ Sources/MIDIKitInternals/Core MIDI/MIDIPacket/MIDIPacket Utilities.swift
@@ -10,23 +10,33 @@ import Foundation
 import CoreMIDI
 
 extension UnsafePointer where Pointee == MIDIPacket {
-    fileprivate static let midiPacketDataOffset: Int = MemoryLayout.offset(of: \MIDIPacket.data)!
-    
     /// Returns the raw bytes of the `MIDIPacket`.
     @_disfavoredOverload
     public var rawBytes: [UInt8] {
-        let packetDataCount = Int(pointee.length)
-        
-        guard packetDataCount > 0 else {
-            return []
-        }
-        
+        MIDIPacket.extractBytes(from: self, count: Int(pointee.length))
+    }
+}
+
+extension UnsafeMutablePointer where Pointee == MIDIPacket {
+    /// Returns the raw bytes of the `MIDIPacket`.
+    @_disfavoredOverload
+    public var rawBytes: [UInt8] {
+        MIDIPacket.extractBytes(from: self, count: Int(pointee.length))
+    }
+}
+
+// MARK: - Helpers
+
+extension MIDIPacket {
+    fileprivate static let midiPacketDataOffset: Int = MemoryLayout.offset(of: \MIDIPacket.data)!
+    
+    fileprivate static func extractBytes(from ptr: UnsafeRawPointer, count: Int) -> [UInt8] {
         // Access the raw memory instead of using the .pointee
         // This workaround is needed due to a variety of crashes that can occur when either the
         // thread sanitizer is on, or large/malformed MIDI packet lists / packets arrive
         let rawMIDIPacketDataPtr = UnsafeRawBufferPointer(
-            start: UnsafeRawPointer(self) + Self.midiPacketDataOffset,
-            count: packetDataCount
+            start: ptr + midiPacketDataOffset,
+            count: count
         )
         
         return [UInt8](rawMIDIPacketDataPtr)
