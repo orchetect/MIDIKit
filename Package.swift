2e17e7fb93760ae8bdf3b9abeeb776a92b7617c0
Steffan Andrews <stef@orchetect.com>
2023-01-16 01:50:07 -0800

    Changed TimecodeKit dependency to release version

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/orchetect/TimecodeKit", branch: "main"),
+        .package(url: "https://github.com/orchetect/TimecodeKit", from: "1.6.4"),
         
         // testing-only:
         .package(url: "https://github.com/orchetect/XCTestUtils", from: "1.0.1")
