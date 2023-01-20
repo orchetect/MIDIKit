35f90f53cd99b9f5b17878c490b83c8abeadf295
Steffan Andrews <stef@orchetect.com>
2023-01-19 17:48:45 -0800

    `MIDIEvent`: logging description now outputs both MIDI 1.0 and MIDI 2.0 values for events.

--- Sources/MIDIKitCore/Events/MIDIEvent/MIDIEvent description.swift
+++ Sources/MIDIKitCore/Events/MIDIEvent/MIDIEvent description.swift
@@ -12,7 +12,7 @@ extension MIDIEvent: CustomStringConvertible, CustomDebugStringConvertible {
         // -------------------
         // MARK: Channel Voice
         // -------------------
-    
+        
         case let .noteOn(event):
             let attrStr: String
             switch event.attribute {
@@ -21,12 +21,18 @@ extension MIDIEvent: CustomStringConvertible, CustomDebugStringConvertible {
             default:
                 attrStr = "\(event.attribute), "
             }
-    
+            
+            let noteString = "\(event.note.number) (\(event.note.stringValue()))"
+            
+            let midi1VelString = "midi1(\(event.velocity.midi1Value))"
+            let midi2VelString = "midi2(\(event.velocity.midi2Value))"
+            let velString = "\(midi1VelString)/\(midi2VelString)"
+            
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
-    
-            return "noteOn(\(event.note.number) (\(event.note.stringValue())), vel: \(event.velocity), \(attrStr)chan: \(channelString), group: \(groupString))"
-    
+            
+            return "noteOn(\(noteString)), vel: \(velString), \(attrStr)chan: \(channelString), group: \(groupString))"
+            
         case let .noteOff(event):
             let attrStr: String
             switch event.attribute {
@@ -35,42 +41,69 @@ extension MIDIEvent: CustomStringConvertible, CustomDebugStringConvertible {
             default:
                 attrStr = "\(event.attribute), "
             }
-    
+            
+            let noteString = "\(event.note.number) (\(event.note.stringValue()))"
+            
+            // only include velocity if > 0
+            var velString: String
+            switch event.velocity {
+            case .midi1(0):
+                velString = ""
+            default:
+                let midi1VelString = "midi1(\(event.velocity.midi1Value))"
+                let midi2VelString = "midi2(\(event.velocity.midi2Value))"
+                velString = "\(midi1VelString)/\(midi2VelString), "
+            }
+            
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
-    
-            return "noteOff(\(event.note.number) (\(event.note.stringValue())), \(attrStr)chan: \(channelString), group: \(groupString))"
-    
+            
+            return "noteOff(\(noteString)), \(velString)\(attrStr)chan: \(channelString), group: \(groupString))"
+            
         case let .noteCC(event):
+            let noteString = "\(event.note.number) (\(event.note.stringValue()))"
+            // value is 32-bit MIDI 2 only, so we can't show a MIDI 1 value since it makes no sense
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
-    
-            return "noteCC(note: \(event.note.number) (\(event.note.stringValue())), controller: \(event.controller), val: \(event.value), chan: \(channelString), group: \(groupString))"
-    
+            
+            return "noteCC(note: \(noteString)), controller: \(event.controller), val: \(event.value), chan: \(channelString), group: \(groupString))"
+            
         case let .notePitchBend(event):
+            let noteString = "\(event.note.number) (\(event.note.stringValue()))"
+            // value is 32-bit MIDI 2 only, so we can't show a MIDI 1 value since it makes no sense
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
-    
-            return "notePitchBend(note: \(event.note.number) (\(event.note.stringValue())), value: \(event.value), chan: \(channelString), group: \(groupString))"
-    
+            
+            return "notePitchBend(note: \(noteString)), value: \(event.value), chan: \(channelString), group: \(groupString))"
+            
         case let .notePressure(event):
+            let noteString = "\(event.note.number) (\(event.note.stringValue()))"
+            
+            let midi1ValString = "midi1(\(event.amount.midi1Value))"
+            let midi2ValString = "midi2(\(event.amount.midi2Value))"
+            let valString = "\(midi1ValString)/\(midi2ValString)"
+            
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
-    
-            return "notePressure(note: \(event.note.number) (\(event.note.stringValue())), amount: \(event.amount), chan: \(channelString), group: \(groupString))"
-    
+            
+            return "notePressure(note: \(noteString)), amount: \(valString), chan: \(channelString), group: \(groupString))"
+            
         case let .noteManagement(event):
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
     
             return "noteManagement(options: \(event.optionFlags), chan: \(channelString), group: \(groupString))"
-    
+            
         case let .cc(event):
+            let midi1ValString = "midi1(\(event.value.midi1Value))"
+            let midi2ValString = "midi2(\(event.value.midi2Value))"
+            let valString = "\(midi1ValString)/\(midi2ValString)"
+            
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
     
-            return "cc(\(event.controller.number), val: \(event.value), chan: \(channelString), group: \(groupString))"
-    
+            return "cc(\(event.controller.number), val: \(valString), chan: \(channelString), group: \(groupString))"
+            
         case let .programChange(event):
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
@@ -81,47 +114,52 @@ extension MIDIEvent: CustomStringConvertible, CustomDebugStringConvertible {
             case let .bankSelect(bank):
                 return "prgChange(\(event.program), bank: \(bank), chan: \(channelString), group: \(groupString))"
             }
-    
+            
         case let .pressure(event):
+            let valString = "midi1(\(event.amount.midi1Value))"
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
-    
-            return "pressure(amount: \(event.amount), chan: \(channelString), group: \(groupString))"
-    
+            
+            return "pressure(amount: \(valString), chan: \(channelString), group: \(groupString))"
+            
         case let .pitchBend(event):
+            let midi1ValString = "midi1(\(event.value.midi1Value))"
+            let midi2ValString = "midi2(\(event.value.midi2Value))"
+            let valString = "\(midi1ValString)/\(midi2ValString)"
+            
             let channelString = event.channel.hexString()
             let groupString = event.group.hexString()
-    
-            return "pitchBend(\(event.value), chan: \(channelString), group: \(groupString))"
-    
+            
+            return "pitchBend(\(valString), chan: \(channelString), group: \(groupString))"
+            
         // ----------------------
         // MARK: System Exclusive
         // ----------------------
-    
+        
         case let .sysEx7(event):
             let dataString = event.data.hexString(padEachTo: 2, prefixes: true)
             let groupString = event.group.hexString()
-    
+            
             return "sysEx7(mfr: \(event.manufacturer), data: [\(dataString)], group: \(groupString))"
-    
+            
         case let .universalSysEx7(event):
             let dataString = event.data.hexString(padEachTo: 2, prefixes: true)
             let groupString = event.group.hexString()
-    
+            
             return "universalSysEx7(\(event.universalType), deviceID: \(event.deviceID), subID1: \(event.subID1), subID2: \(event.subID2), data: [\(dataString)], group: \(groupString))"
-    
+            
         case let .sysEx8(event):
             let dataString = event.data.hexString(padEachTo: 2, prefixes: true)
             let groupString = event.group.hexString()
-    
+            
             return "sysEx8(mfr: \(event.manufacturer), data: [\(dataString)], group: \(groupString), streamID: \(event.streamID))"
-    
+            
         case let .universalSysEx8(event):
             let dataString = event.data.hexString(padEachTo: 2, prefixes: true)
             let groupString = event.group.hexString()
-    
+            
             return "universalSysEx8(\(event.universalType), deviceID: \(event.deviceID), subID1: \(event.subID1), subID2: \(event.subID2), data: [\(dataString)], group: \(groupString), streamID: \(event.streamID))"
-    
+            
         // -------------------
         // MARK: System Common
         // -------------------
@@ -129,67 +167,67 @@ extension MIDIEvent: CustomStringConvertible, CustomDebugStringConvertible {
         case let .timecodeQuarterFrame(event):
             let dataByteString = event.dataByte.uInt8Value
                 .binaryString(padTo: 8, prefix: true)
-    
+            
             let groupString = event.group.hexString()
-    
+            
             return "timecodeQF(\(dataByteString), group: \(groupString))"
-    
+            
         case let .songPositionPointer(event):
             let groupString = event.group.hexString()
             return "songPositionPointer(beat: \(event.midiBeat), group: \(groupString))"
-    
+            
         case let .songSelect(event):
             let groupString = event.group.hexString()
             return "songSelect(number: \(event.number), group: \(groupString))"
-    
+            
         case let .unofficialBusSelect(event):
             let groupString = event.group.hexString()
             return "unofficialBusSelect(bus: \(event.bus), group: \(groupString))"
-    
+            
         case let .tuneRequest(event):
             let groupString = event.group.hexString()
             return "tuneRequest(group: \(groupString))"
-    
+        
         // ----------------------
         // MARK: System Real-Time
         // ----------------------
-    
+        
         case let .timingClock(event):
             let groupString = event.group.hexString()
             return "timingClock(group: \(groupString))"
-    
+            
         case let .start(event):
             let groupString = event.group.hexString()
             return "start(group: \(groupString))"
-    
+            
         case let .continue(event):
             let groupString = event.group.hexString()
             return "continue(group: \(groupString))"
-    
+            
         case let .stop(event):
             let groupString = event.group.hexString()
             return "stop(group: \(groupString))"
-    
+            
         case let .activeSensing(event):
             let groupString = event.group.hexString()
             return "activeSensing(group: \(groupString))"
-    
+            
         case let .systemReset(event):
             let groupString = event.group.hexString()
             return "systemReset(group: \(groupString))"
-    
+            
         // -------------------------------
         // MARK: MIDI 2.0 Utility Messages
         // -------------------------------
-    
+        
         case let .noOp(event):
             let groupString = event.group.hexString()
             return "noOp(group: \(groupString))"
-    
+            
         case let .jrClock(event):
             let groupString = event.group.hexString()
             return "jrClock(\(event.time), group: \(groupString))"
-    
+            
         case let .jrTimestamp(event):
             let groupString = event.group.hexString()
             return "jrTimestamp(\(event.time), group: \(groupString))"
