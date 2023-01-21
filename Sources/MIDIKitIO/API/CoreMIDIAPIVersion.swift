dc71a2bfbc1232f7590fd9472e10b5605706aedd
Steffan Andrews <stef@orchetect.com>
2023-01-20 18:05:13 -0800

    Allow `legacyCoreMIDI` on macOS 13+ and iOS 16+

--- Sources/MIDIKitIO/API/CoreMIDIAPIVersion.swift
+++ Sources/MIDIKitIO/API/CoreMIDIAPIVersion.swift
@@ -51,13 +51,13 @@ extension CoreMIDIAPIVersion {
         case .legacyCoreMIDI:
             #if os(macOS)
                 // Apple has deprecated legacy API but not yet obsoleted.
-                // We'll guess that it may become obsoleted as of macOS 13.0
-                if #available(macOS 13, *) { return false }
+                // TODO: Once it becomes known what macOS version fully obsoletes it, update this:
+                //if #available(macOS 14, *) { return false }
                 return true
             #elseif os(iOS)
                 // Apple has deprecated legacy API but not yet obsoleted.
-                // We'll guess that it may become obsoleted as of iOS 16.0
-                if #available(iOS 16, *) { return false }
+            // TODO: Once it becomes known what iOS version fully obsoletes it, update this:
+                //if #available(iOS 17, *) { return false }
                 return true
             #elseif os(tvOS) || os(watchOS)
                 // only new API is supported on tvOS and watchOS,
