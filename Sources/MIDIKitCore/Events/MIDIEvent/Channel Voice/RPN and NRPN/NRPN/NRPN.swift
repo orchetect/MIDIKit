9e12ff23d1993bc0e13fde01bafd03b5e3926a58
Steffan Andrews <stef@orchetect.com>
2023-01-20 19:43:32 -0800

    Added additional data value static constructors for RPN/NRPN

--- Sources/MIDIKitCore/Events/MIDIEvent/Channel Voice/RPN and NRPN/NRPN/NRPN.swift
+++ Sources/MIDIKitCore/Events/MIDIEvent/Channel Voice/RPN and NRPN/NRPN/NRPN.swift
@@ -197,7 +197,132 @@ extension MIDIEvent {
         )
     }
     
-    // MARK: - Extra Static Constructors
+    // note: this comment block should be duplicated to the places noted at the top of this file
+    
+    /// Channel Voice Message: NRPN (Non-Registered Parameter Number),
+    /// also referred to as Assignable Controller in MIDI 2.0.
+    /// (MIDI 1.0 / MIDI 2.0)
+    ///
+    /// > MIDI 1.0 Spec:
+    /// >
+    /// > In order to set or change the value of a Assignable Parameter (NRPN), the following
+    /// > occurs:
+    /// >
+    /// > 1. Two Control Change messages are sent using CC 99 (0x63) and 98 (0x62) to select the
+    /// > desired Assignable Parameter Number.
+    /// >
+    /// > 2. When setting the Assignable Parameter to a specific value, CC messages are sent to the
+    /// > Data Entry MSB controller (CC 6). If the selected Registered Parameter requires the LSB to
+    /// > be set, another CC message is sent to the Data Entry LSB controller (CC 38).
+    /// >
+    /// > 3. To make a relative adjustment to the selected Assignable Parameter's current value, use
+    /// > the Data Increment or Data Decrement controllers (CCs 96 & 97).
+    /// >
+    /// > For registered Parameter Number use, see RPN (Registered Parameter Numbers).
+    ///
+    /// > MIDI 2.0 Spec:
+    /// >
+    /// > In the MIDI 2.0 Protocol, Registered Controllers (RPN) and Assignable Controllers (NRPN)
+    /// > use a single, unified message, making them much easier to use.
+    /// >
+    /// > As a result, CC 6, 38, 98, 99, 100, and 101 are not to be used in standalone CC messages,
+    /// > as the new MIDI 2.0 RPN/NRPN UMP messages replace them.
+    /// >
+    /// > Assignable Controllers (NRPNs) have no specific function and are available for any device
+    /// > or application-specific function. Assignable Controllers map and translate directly to
+    /// > MIDI 1.0 Non-Registered Parameter Numbers (NRPN). Assignable Controllers are also
+    /// > organized in 128 Banks (corresponds to NRPN MSB), with 128 controllers per Bank
+    /// > (corresponds to NRPN LSB).
+    ///
+    /// See Recommended Practise [RP-018](https://www.midi.org/specifications/midi1-specifications/midi-1-addenda/response-to-data-increment-decrement-controllers)
+    /// of the MIDI 1.0 Spec Addenda.
+    ///
+    /// - Parameters:
+    ///   - parameter: Non-Registered Parameter Number (Assignable Controller).
+    ///   - data: MIDI 1.0 14-bit data value.
+    ///   - change: Determines whether the value is absolute or a relative change. MIDI 1.0 is always absolute.
+    ///   - channel: Channel Number (`0x0 ... 0xF`)
+    ///   - group: UMP Group (`0x0 ... 0xF`)
+    public static func nrpn(
+        parameter: UInt7Pair,
+        data: UInt14,
+        change: MIDI2ParameterNumberChange = .absolute,
+        channel: UInt4,
+        group: UInt4 = 0x0
+    ) -> Self {
+        let dataPair = data.midiUInt7Pair
+        let dataTuple = (msb: dataPair.msb, lsb: dataPair.lsb)
+        return .nrpn(
+            AssignableController(parameter: parameter, data: dataTuple),
+            change: change,
+            channel: channel,
+            group: group
+        )
+    }
+    
+    // note: this comment block should be duplicated to the places noted at the top of this file
+    
+    /// Channel Voice Message: NRPN (Non-Registered Parameter Number),
+    /// also referred to as Assignable Controller in MIDI 2.0.
+    /// (MIDI 1.0 / MIDI 2.0)
+    ///
+    /// > MIDI 1.0 Spec:
+    /// >
+    /// > In order to set or change the value of a Assignable Parameter (NRPN), the following
+    /// > occurs:
+    /// >
+    /// > 1. Two Control Change messages are sent using CC 99 (0x63) and 98 (0x62) to select the
+    /// > desired Assignable Parameter Number.
+    /// >
+    /// > 2. When setting the Assignable Parameter to a specific value, CC messages are sent to the
+    /// > Data Entry MSB controller (CC 6). If the selected Registered Parameter requires the LSB to
+    /// > be set, another CC message is sent to the Data Entry LSB controller (CC 38).
+    /// >
+    /// > 3. To make a relative adjustment to the selected Assignable Parameter's current value, use
+    /// > the Data Increment or Data Decrement controllers (CCs 96 & 97).
+    /// >
+    /// > For registered Parameter Number use, see RPN (Registered Parameter Numbers).
+    ///
+    /// > MIDI 2.0 Spec:
+    /// >
+    /// > In the MIDI 2.0 Protocol, Registered Controllers (RPN) and Assignable Controllers (NRPN)
+    /// > use a single, unified message, making them much easier to use.
+    /// >
+    /// > As a result, CC 6, 38, 98, 99, 100, and 101 are not to be used in standalone CC messages,
+    /// > as the new MIDI 2.0 RPN/NRPN UMP messages replace them.
+    /// >
+    /// > Assignable Controllers (NRPNs) have no specific function and are available for any device
+    /// > or application-specific function. Assignable Controllers map and translate directly to
+    /// > MIDI 1.0 Non-Registered Parameter Numbers (NRPN). Assignable Controllers are also
+    /// > organized in 128 Banks (corresponds to NRPN MSB), with 128 controllers per Bank
+    /// > (corresponds to NRPN LSB).
+    ///
+    /// See Recommended Practise [RP-018](https://www.midi.org/specifications/midi1-specifications/midi-1-addenda/response-to-data-increment-decrement-controllers)
+    /// of the MIDI 1.0 Spec Addenda.
+    ///
+    /// - Parameters:
+    ///   - parameter: Non-Registered Parameter Number (Assignable Controller).
+    ///   - data: MIDI 2.0 32-bit data value. Will be scaled down to 14-bit when necessary.
+    ///   - change: Determines whether the value is absolute or a relative change. MIDI 1.0 is always absolute.
+    ///   - channel: Channel Number (`0x0 ... 0xF`)
+    ///   - group: UMP Group (`0x0 ... 0xF`)
+    public static func nrpn(
+        parameter: UInt7Pair,
+        data: UInt32,
+        change: MIDI2ParameterNumberChange = .absolute,
+        channel: UInt4,
+        group: UInt4 = 0x0
+    ) -> Self {
+        let downscaledData = ChanVoice14Bit32BitValue.midi2(data).midi1Value
+        let dataPair = downscaledData.midiUInt7Pair
+        let dataTuple = (msb: dataPair.msb, lsb: dataPair.lsb)
+        return .nrpn(
+            AssignableController(parameter: parameter, data: dataTuple),
+            change: change,
+            channel: channel,
+            group: group
+        )
+    }
     
     /// Assembles a MIDI 1.0 NRPN compound message, consisting of multiple CC events.
     ///
--- Sources/MIDIKitCore/Events/MIDIEvent/Channel Voice/RPN and NRPN/RPN/RPN.swift
+++ Sources/MIDIKitCore/Events/MIDIEvent/Channel Voice/RPN and NRPN/RPN/RPN.swift
@@ -184,7 +184,7 @@ extension MIDIEvent {
     ///
     /// - Parameters:
     ///   - parameter: Registered Parameter Number (Registered Controller).
-    ///   - data: Two 7-bit data bytes, optional.
+    ///   - data: Two 7-bit MIDI 1.0 data bytes, optional.
     ///   - change: Determines whether the value is absolute or a relative change. MIDI 1.0 is always absolute.
     ///   - channel: Channel Number (`0x0 ... 0xF`)
     ///   - group: UMP Group (`0x0 ... 0xF`)
@@ -203,7 +203,136 @@ extension MIDIEvent {
         )
     }
     
-    // MARK: - Extra Static Constructors
+    // note: this comment block should be duplicated to the places noted at the top of this file
+    
+    /// Channel Voice Message: RPN (Registered Parameter Number),
+    /// also referred to as Registered Controller in MIDI 2.0.
+    /// (MIDI 1.0 / MIDI 2.0)
+    ///
+    /// > MIDI 1.0 Spec:
+    /// >
+    /// > In order to set or change the value of a Registered Parameter (RPN), the following occurs:
+    /// >
+    /// > 1. Two Control Change messages are sent using CC 101 (0x65) and 100 (0x64) to select the
+    /// > desired Registered Parameter Number.
+    /// >
+    /// > 2. When setting the Registered Parameter to a specific value, CC messages are sent to the
+    /// > Data Entry MSB controller (CC 6). If the selected Registered Parameter requires the LSB to
+    /// > be set, another CC message is sent to the Data Entry LSB controller (CC 38).
+    /// >
+    /// > 3. To make a relative adjustment to the selected Registered Parameter's current value, use
+    /// > the Data Increment or Data Decrement controllers (CCs 96 & 97).
+    /// >
+    /// > Currently undefined RPN parameter numbers are all RESERVED for future MMA Definition.
+    /// >
+    /// > For custom Parameter Number use, see NRPN (Non-Registered Parameter Numbers).
+    ///
+    /// > MIDI 2.0 Spec:
+    /// >
+    /// > In the MIDI 2.0 Protocol, Registered Controllers (RPN) and Assignable Controllers (NRPN)
+    /// > use a single, unified message, making them much easier to use.
+    /// >
+    /// > As a result, CC 6, 38, 98, 99, 100, and 101 are not to be used in standalone CC messages,
+    /// > as the new MIDI 2.0 RPN/NRPN UMP messages replace them.
+    /// >
+    /// > Registered Controllers (RPNs) have specific functions defined by MMA/AMEI specifications.
+    /// > Registered Controllers map and translate directly to MIDI 1.0 Registered Parameter Numbers
+    /// > (RPN, see Appendix D.2.3) and use the same definitions as MMA/AMEI approved RPN messages.
+    /// > Registered Controllers are organized in 128 Banks (corresponds to RPN MSB), with 128
+    /// > controllers per Bank (corresponds to RPN LSB).
+    ///
+    /// - Note: See Recommended Practise
+    /// [RP-018](https://www.midi.org/specifications/midi1-specifications/midi-1-addenda/response-to-data-increment-decrement-controllers)
+    /// of the MIDI 1.0 Spec Addenda.
+    ///
+    /// - Parameters:
+    ///   - parameter: Registered Parameter Number (Registered Controller).
+    ///   - data: MIDI 1.0 14-bit data value.
+    ///   - change: Determines whether the value is absolute or a relative change. MIDI 1.0 is always absolute.
+    ///   - channel: Channel Number (`0x0 ... 0xF`)
+    ///   - group: UMP Group (`0x0 ... 0xF`)
+    public static func rpn(
+        parameter: UInt7Pair,
+        data: UInt14,
+        change: MIDI2ParameterNumberChange = .absolute,
+        channel: UInt4,
+        group: UInt4 = 0x0
+    ) -> Self {
+        let dataPair = data.midiUInt7Pair
+        let dataTuple = (msb: dataPair.msb, lsb: dataPair.lsb)
+        return .rpn(
+            RegisteredController(parameter: parameter, data: dataTuple),
+            change: change,
+            channel: channel,
+            group: group
+        )
+    }
+    
+    // note: this comment block should be duplicated to the places noted at the top of this file
+    
+    /// Channel Voice Message: RPN (Registered Parameter Number),
+    /// also referred to as Registered Controller in MIDI 2.0.
+    /// (MIDI 1.0 / MIDI 2.0)
+    ///
+    /// > MIDI 1.0 Spec:
+    /// >
+    /// > In order to set or change the value of a Registered Parameter (RPN), the following occurs:
+    /// >
+    /// > 1. Two Control Change messages are sent using CC 101 (0x65) and 100 (0x64) to select the
+    /// > desired Registered Parameter Number.
+    /// >
+    /// > 2. When setting the Registered Parameter to a specific value, CC messages are sent to the
+    /// > Data Entry MSB controller (CC 6). If the selected Registered Parameter requires the LSB to
+    /// > be set, another CC message is sent to the Data Entry LSB controller (CC 38).
+    /// >
+    /// > 3. To make a relative adjustment to the selected Registered Parameter's current value, use
+    /// > the Data Increment or Data Decrement controllers (CCs 96 & 97).
+    /// >
+    /// > Currently undefined RPN parameter numbers are all RESERVED for future MMA Definition.
+    /// >
+    /// > For custom Parameter Number use, see NRPN (Non-Registered Parameter Numbers).
+    ///
+    /// > MIDI 2.0 Spec:
+    /// >
+    /// > In the MIDI 2.0 Protocol, Registered Controllers (RPN) and Assignable Controllers (NRPN)
+    /// > use a single, unified message, making them much easier to use.
+    /// >
+    /// > As a result, CC 6, 38, 98, 99, 100, and 101 are not to be used in standalone CC messages,
+    /// > as the new MIDI 2.0 RPN/NRPN UMP messages replace them.
+    /// >
+    /// > Registered Controllers (RPNs) have specific functions defined by MMA/AMEI specifications.
+    /// > Registered Controllers map and translate directly to MIDI 1.0 Registered Parameter Numbers
+    /// > (RPN, see Appendix D.2.3) and use the same definitions as MMA/AMEI approved RPN messages.
+    /// > Registered Controllers are organized in 128 Banks (corresponds to RPN MSB), with 128
+    /// > controllers per Bank (corresponds to RPN LSB).
+    ///
+    /// - Note: See Recommended Practise
+    /// [RP-018](https://www.midi.org/specifications/midi1-specifications/midi-1-addenda/response-to-data-increment-decrement-controllers)
+    /// of the MIDI 1.0 Spec Addenda.
+    ///
+    /// - Parameters:
+    ///   - parameter: Registered Parameter Number (Registered Controller).
+    ///   - data: MIDI 2.0 32-bit data value. Will be scaled down to 14-bit when necessary.
+    ///   - change: Determines whether the value is absolute or a relative change. MIDI 1.0 is always absolute.
+    ///   - channel: Channel Number (`0x0 ... 0xF`)
+    ///   - group: UMP Group (`0x0 ... 0xF`)
+    public static func rpn(
+        parameter: UInt7Pair,
+        data: UInt32,
+        change: MIDI2ParameterNumberChange = .absolute,
+        channel: UInt4,
+        group: UInt4 = 0x0
+    ) -> Self {
+        let downscaledData = ChanVoice14Bit32BitValue.midi2(data).midi1Value
+        let dataPair = downscaledData.midiUInt7Pair
+        let dataTuple = (msb: dataPair.msb, lsb: dataPair.lsb)
+        return .rpn(
+            RegisteredController(parameter: parameter, data: dataTuple),
+            change: change,
+            channel: channel,
+            group: group
+        )
+    }
     
     /// Assembles a MIDI 1.0 RPN compound message, consisting of multiple CC events.
     ///
