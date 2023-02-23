3707fa0b1390344699a8d450a28c918c04037b38
Steffan Andrews <stef@orchetect.com>
2023-02-23 13:10:17 -0800

    Updated unit tests

---
+++ Tests/MIDIKitIOTests/Core MIDI/MIDIPacket/MIDIPacket Tests.swift
@@ -0,0 +1,43 @@
+//
+//  MIDIPacket Tests.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2023 Steffan Andrews • Licensed under MIT License
+//
+
+#if shouldTestCurrentPlatform && !os(tvOS) && !os(watchOS)
+
+import XCTest
+@testable import MIDIKitIO
+import CoreMIDI
+
+final class MIDIPacket_Tests: XCTestCase {
+    func testEmptyBytes256Length() throws {
+        let testPacket = kMIDIPacket.emptyBytes256Length
+        
+        XCTAssertEqual(testPacket.rawBytes, [UInt8](repeating: 0x00, count: 256))
+        XCTAssertEqual(testPacket.rawTimeStamp, 123_456_789)
+    }
+    
+    func testNoteOn60Vel65Chan1() throws {
+        let testPacket = kMIDIPacket.noteOn60Vel65Chan1
+        
+        XCTAssertEqual(testPacket.rawBytes, [0x90, 0x3C, 0x41])
+        XCTAssertEqual(testPacket.rawTimeStamp, 123_456_789)
+    }
+    
+    func testNoteOn60Vel65Chan1_CC12Val105Chan1() throws {
+        let testPacket = kMIDIPacket.noteOn60Vel65Chan1_CC12Val105Chan1
+        
+        XCTAssertEqual(testPacket.rawBytes, [0x90, 0x3C, 0x41, 0xB0, 0x08, 0x69])
+        XCTAssertEqual(testPacket.rawTimeStamp, 987_654_321)
+    }
+    
+    func testFull256Bytes() throws {
+        let testPacket = kMIDIPacket.full256Bytes
+        
+        XCTAssertEqual(testPacket.rawBytes, kMIDIPacket.full256Bytes_rawBytes)
+        XCTAssertEqual(testPacket.rawTimeStamp, UInt64.max)
+    }
+}
+
+#endif
---
+++ Tests/MIDIKitIOTests/Packet/MIDIPacketData Tests.swift
@@ -0,0 +1,47 @@
+//
+//  MIDIPacketData Tests.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2023 Steffan Andrews • Licensed under MIT License
+//
+
+#if shouldTestCurrentPlatform && !os(tvOS) && !os(watchOS)
+
+import XCTest
+@testable import MIDIKitIO
+import CoreMIDI
+
+final class MIDIPacketData_Tests: XCTestCase {
+    func testEmptyBytes256Length() throws {
+        var testPacket = kMIDIPacket.emptyBytes256Length
+        let data = MIDIPacketData(&testPacket, refCon: nil, refConKnown: false)
+        
+        XCTAssertEqual(data.bytes, [UInt8](repeating: 0x00, count: 256))
+        XCTAssertEqual(data.timeStamp, 123_456_789)
+    }
+    
+    func testNoteOn60Vel65Chan1() throws {
+        var testPacket = kMIDIPacket.noteOn60Vel65Chan1
+        let data = MIDIPacketData(&testPacket, refCon: nil, refConKnown: false)
+        
+        XCTAssertEqual(data.bytes, [0x90, 0x3C, 0x41])
+        XCTAssertEqual(data.timeStamp, 123_456_789)
+    }
+    
+    func testNoteOn60Vel65Chan1_CC12Val105Chan1() throws {
+        var testPacket = kMIDIPacket.noteOn60Vel65Chan1_CC12Val105Chan1
+        let data = MIDIPacketData(&testPacket, refCon: nil, refConKnown: false)
+        
+        XCTAssertEqual(data.bytes, [0x90, 0x3C, 0x41, 0xB0, 0x08, 0x69])
+        XCTAssertEqual(data.timeStamp, 987_654_321)
+    }
+    
+    func testFull256Bytes() throws {
+        var testPacket = kMIDIPacket.full256Bytes
+        let data = MIDIPacketData(&testPacket, refCon: nil, refConKnown: false)
+        
+        XCTAssertEqual(data.bytes, kMIDIPacket.full256Bytes_rawBytes)
+        XCTAssertEqual(data.timeStamp, UInt64.max)
+    }
+}
+
+#endif
