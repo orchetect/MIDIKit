1f9c30bd588b9c7634c92b1a6b5d96803676cc13
Steffan Andrews <stef@orchetect.com>
2023-02-23 18:09:23 -0800

    `MIDIDevices`: Added `inputs` and `outputs` convenience properties to return endpoint dictionaries

--- Sources/MIDIKitIO/MIDIDevices/MIDIDevices.swift
+++ Sources/MIDIKitIO/MIDIDevices/MIDIDevices.swift
@@ -11,13 +11,40 @@ import Foundation
 // this protocol may not be necessary, it was experimental so that the `MIDIManager.devices`
 // property could be swapped out with a different Devices class with Combine support
 public protocol MIDIDevicesProtocol {
-    /// List of MIDI devices in the system
+    /// List of MIDI devices in the system.
+    ///
+    /// A device can contain zero or more entities, and an entity can contain zero or more inputs
+    /// and output endpoints.
     var devices: [MIDIDevice] { get }
     
     /// Manually update the locally cached contents from the system.
     /// This method does not need to be manually invoked, as it is handled internally when MIDI
     /// system endpoints change.
     func updateCachedProperties()
+}
+
+extension MIDIDevicesProtocol {
+    /// Returns a dictionary keyed by device with value of an array containing all the input
+    /// endpoints for the device. (Convenience)
+    ///
+    /// A device can contain zero or more entities, and an entity can contain zero or more inputs
+    /// and output endpoints.
+    public var inputs: [MIDIDevice: [MIDIInputEndpoint]] {
+        devices.reduce(into: [:]) { dict, device in
+            dict[device] = device.entities.flatMap { $0.inputs }
+        }
+    }
+    
+    /// Returns a dictionary keyed by device with value of an array containing all the output
+    /// endpoints for the device. (Convenience)
+    ///
+    /// A device can contain zero or more entities, and an entity can contain zero or more inputs
+    /// and output endpoints.
+    public var outputs: [MIDIDevice: [MIDIOutputEndpoint]] {
+        devices.reduce(into: [:]) { dict, device in
+            dict[device] = device.entities.flatMap { $0.outputs }
+        }
+    }
 }
 
 /// Manages system MIDI devices information cache.
