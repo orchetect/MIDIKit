93fe71046bc794da7241fd198b31dfa72beede98
Steffan Andrews <stef@orchetect.com>
2023-05-03 21:37:41 -0700

    Cleanup

--- Examples/SwiftUI Multiplatform/EndpointPickers/EndpointPickers/Utilities.swift
+++ Examples/SwiftUI Multiplatform/EndpointPickers/EndpointPickers/Utilities.swift
@@ -7,21 +7,6 @@
 import SwiftUI
 import MIDIKitIO
 
-/// Wrapper to convert between `Int` and another integer type
-/// since `@AppStorage` only supports `Int`.
-func intAdapter<T: BinaryInteger>(_ storage: Binding<Int?>) -> Binding<T?> {
-    Binding {
-        guard let storage = storage.wrappedValue else { return nil }
-        return T(exactly: storage)
-    } set: { new in
-        guard let new else {
-            storage.wrappedValue = nil
-            return
-        }
-        storage.wrappedValue = Int(new)
-    }
-}
-
 /// Allow use with `@AppStorage` by conforming to a supported `RawRepresentable` type.
 extension MIDIIdentifier: RawRepresentable {
     public typealias RawValue = Int
