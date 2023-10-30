adb811c7a907d9d82474144bb7b7028f84b73a56
Steffan Andrews <stef@orchetect.com>
2023-10-29 19:11:07 -0700

    0.9.3 API Evolution

---
+++ Sources/MIDIKitIO/API Evolution/MIDIKit-0.9.3.swift
@@ -0,0 +1,93 @@
+//
+//  MIDIKit-0.9.3.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2023 Steffan Andrews • Licensed under MIT License
+//
+
+import Foundation
+
+extension MIDIReceiver {
+    @available(
+        *,
+        deprecated,
+        renamed: "events(options:_:)",
+        message: "`translateMIDI1NoteOnZeroVelocityToNoteOff` property is now an OptionSet set flag."
+    )
+    @_disfavoredOverload
+    public static func events(
+        translateMIDI1NoteOnZeroVelocityToNoteOff: Bool = true,
+        _ handler: @escaping EventsHandler
+    ) -> Self {
+        .events(
+            options: translateMIDI1NoteOnZeroVelocityToNoteOff ? [.translateMIDI1NoteOnZeroVelocityToNoteOff] : [],
+            handler
+        )
+    }
+    
+    @available(
+        *,
+        deprecated,
+        renamed: "eventsWithMetadata(options:_:)",
+        message: "`translateMIDI1NoteOnZeroVelocityToNoteOff` property is now an OptionSet flag."
+    )
+    @_disfavoredOverload
+    public static func eventsWithMetadata(
+        translateMIDI1NoteOnZeroVelocityToNoteOff: Bool = true,
+        _ handler: @escaping EventsWithMetadataHandler
+    ) -> Self {
+        .eventsWithMetadata(
+            options: translateMIDI1NoteOnZeroVelocityToNoteOff ? [.translateMIDI1NoteOnZeroVelocityToNoteOff] : [],
+            handler
+        )
+    }
+    
+    @available(
+        *,
+        deprecated,
+        renamed: "eventsLogging(options:_:)",
+        message: "`filterActiveSensingAndClock` property is now an OptionSet flag."
+    )
+    @_disfavoredOverload
+    public static func eventsLogging(
+        filterActiveSensingAndClock: Bool = false,
+        _ handler: EventsLoggingHandler? = nil
+    ) -> Self {
+        .eventsLogging(
+            options: filterActiveSensingAndClock ? [.filterActiveSensingAndClock] : [],
+            handler
+        )
+    }
+    
+    @available(
+        *,
+        deprecated,
+        renamed: "rawDataLogging(_:)",
+        message: "`filterActiveSensingAndClock` has been removed from rawDataLogging."
+    )
+    @_disfavoredOverload
+    public static func rawDataLogging(
+        filterActiveSensingAndClock: Bool = false,
+        _ handler: RawDataLoggingHandler? = nil
+    ) -> Self {
+        .rawDataLogging(handler)
+    }
+    
+    @available(
+        *,
+         deprecated,
+         renamed: "object(_:held:options:)",
+         message: "`translateMIDI1NoteOnZeroVelocityToNoteOff` has been removed from rawDataLogging."
+    )
+    @_disfavoredOverload
+    public static func object(
+        _ object: ReceivesMIDIEvents,
+        held: ReceiverRefStorage,
+        translateMIDI1NoteOnZeroVelocityToNoteOff: Bool = true
+    ) -> Self {
+        .object(
+            object,
+            held: held,
+            options: translateMIDI1NoteOnZeroVelocityToNoteOff ? [.translateMIDI1NoteOnZeroVelocityToNoteOff] : []
+        )
+    }
+}
