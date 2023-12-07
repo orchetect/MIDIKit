71052a7f2401962c15e62f4c19eb52ee89d312bf
Steffan Andrews <stef@orchetect.com>
2023-12-06 17:06:02 -0800

    Deflaked unit tests

--- Tests/MIDIKitIOTests/Integration Tests/RPN NRPN IO Tests.swift
+++ Tests/MIDIKitIOTests/Integration Tests/RPN NRPN IO Tests.swift
@@ -232,10 +232,35 @@ final class RPN_NRPN_IO_Tests: XCTestCase {
             0xB2, 0x06, 0x10 // data entry MSB value 0x10
         ])
         
-        // wait(sec: 1.0)
-        wait(for: receivedEvents.count == 1, timeout: 1.0)
+        // Unit test de-flake:
+        // Filter received events in order to ignore a spurious initial CC message.
+        //
+        // Often systems that run these unit tests are slow or bogged down, so the discrete MIDI 1.0
+        // packets may be fired with slightly too much delay between packets.
+        // Core MIDI allows individual CC 0,6,32,38,98,99,100,101 messages to be received from a
+        // MIDI 1.0 device/endpoint and be delivered to a MIDI 2.0 consumer.
+        // But Core MIDI also has a heuristic which attempts to detect a series of RPN/NRPN CC
+        // messages in discrete packets -- but only if it receives these messages in short enough
+        // succession with nearly no delay between them. Then it will drop the individual CC
+        // messages and produce a single MIDI 2.0 RPN/NRPN UMP packet containing all of the
+        // pertinent bytes.
+        // However, what happens when the MIDI 1.0 packets are sent with too much delay between them
+        // is Core MIDI will pass the first CC message through as-is, assuming that it is a discrete
+        // CC message. but once it sees the second CC message that defines the 2nd message in a
+        // 2-message RPN/NRPN header (CC 101 and CC 100, or CC 99 and CC 98) then it realizes that
+        // this 2nd message and any Data Entry MSB (CC 0) that follows should be all packaged up
+        // into a MIDI 2.0 RPN/NRPN UMP packet.
+        // The result is that we receive either a CC 101 or 99, followed by the expected RPN/NRPN
+        // event.
+        // The solution for de-flaking the unit test is to just ignore all non-RPN/NRPN events.
+        func filteredEvents() -> [MIDIEvent] {
+            receivedEvents.filter(chanVoice: .keepType(.rpn))
+        }
         
-        XCTAssertEqual(receivedEvents, [
+        // wait(sec: 1.0)
+        wait(for: filteredEvents().count == 1, timeout: 1.0)
+        
+        XCTAssertEqual(filteredEvents(), [
             .rpn(parameter: .init(msb: 0x40, lsb: 0x41),
                  data: (msb: 0x10, lsb: 0x00),
                  channel: 0x02)
@@ -275,12 +300,37 @@ final class RPN_NRPN_IO_Tests: XCTestCase {
             0xB2, 0x26, 0x20 // data entry LSB value 0x20
         ])
         
+        // Unit test de-flake:
+        // Filter received events in order to ignore a spurious initial CC message.
+        //
+        // Often systems that run these unit tests are slow or bogged down, so the discrete MIDI 1.0
+        // packets may be fired with slightly too much delay between packets.
+        // Core MIDI allows individual CC 0,6,32,38,98,99,100,101 messages to be received from a
+        // MIDI 1.0 device/endpoint and be delivered to a MIDI 2.0 consumer.
+        // But Core MIDI also has a heuristic which attempts to detect a series of RPN/NRPN CC
+        // messages in discrete packets -- but only if it receives these messages in short enough
+        // succession with nearly no delay between them. Then it will drop the individual CC
+        // messages and produce a single MIDI 2.0 RPN/NRPN UMP packet containing all of the
+        // pertinent bytes.
+        // However, what happens when the MIDI 1.0 packets are sent with too much delay between them
+        // is Core MIDI will pass the first CC message through as-is, assuming that it is a discrete
+        // CC message. but once it sees the second CC message that defines the 2nd message in a
+        // 2-message RPN/NRPN header (CC 101 and CC 100, or CC 99 and CC 98) then it realizes that
+        // this 2nd message and any Data Entry MSB (CC 0) that follows should be all packaged up
+        // into a MIDI 2.0 RPN/NRPN UMP packet.
+        // The result is that we receive either a CC 101 or 99, followed by the expected RPN/NRPN
+        // event.
+        // The solution for de-flaking the unit test is to just ignore all non-RPN/NRPN events.
+        func filteredEvents() -> [MIDIEvent] {
+            receivedEvents.filter(chanVoice: .keepType(.rpn))
+        }
+        
         // wait(sec: 1.0)
-        wait(for: receivedEvents.count == 2, timeout: 1.0)
+        wait(for: filteredEvents().count == 2, timeout: 1.0)
         
         // Core MIDI translates MIDI 1.0 NRPN to a MIDI 2.0 UMP packet with MSB first,
         // then a second UMP packet adding the LSB to the same base packet data.
-        XCTAssertEqual(receivedEvents, [
+        XCTAssertEqual(filteredEvents(), [
             .rpn(parameter: .init(msb: 0x40, lsb: 0x41),
                  data: (msb: 0x10, lsb: 0x00),
                  channel: 0x02),
@@ -393,10 +443,35 @@ final class RPN_NRPN_IO_Tests: XCTestCase {
             0xB2, 0x06, 0x10 // data entry MSB value 0x10
         ])
         
-        // wait(sec: 1.0)
-        wait(for: receivedEvents.count == 1, timeout: 1.0)
+        // Unit test de-flake:
+        // Filter received events in order to ignore a spurious initial CC message.
+        //
+        // Often systems that run these unit tests are slow or bogged down, so the discrete MIDI 1.0
+        // packets may be fired with slightly too much delay between packets.
+        // Core MIDI allows individual CC 0,6,32,38,98,99,100,101 messages to be received from a
+        // MIDI 1.0 device/endpoint and be delivered to a MIDI 2.0 consumer.
+        // But Core MIDI also has a heuristic which attempts to detect a series of RPN/NRPN CC
+        // messages in discrete packets -- but only if it receives these messages in short enough
+        // succession with nearly no delay between them. Then it will drop the individual CC
+        // messages and produce a single MIDI 2.0 RPN/NRPN UMP packet containing all of the
+        // pertinent bytes.
+        // However, what happens when the MIDI 1.0 packets are sent with too much delay between them
+        // is Core MIDI will pass the first CC message through as-is, assuming that it is a discrete
+        // CC message. but once it sees the second CC message that defines the 2nd message in a
+        // 2-message RPN/NRPN header (CC 101 and CC 100, or CC 99 and CC 98) then it realizes that
+        // this 2nd message and any Data Entry MSB (CC 0) that follows should be all packaged up
+        // into a MIDI 2.0 RPN/NRPN UMP packet.
+        // The result is that we receive either a CC 101 or 99, followed by the expected RPN/NRPN
+        // event.
+        // The solution for de-flaking the unit test is to just ignore all non-RPN/NRPN events.
+        func filteredEvents() -> [MIDIEvent] {
+            receivedEvents.filter(chanVoice: .keepType(.nrpn))
+        }
         
-        XCTAssertEqual(receivedEvents, [
+        // wait(sec: 1.0)
+        wait(for: filteredEvents().count == 1, timeout: 1.0)
+        
+        XCTAssertEqual(filteredEvents(), [
             .nrpn(parameter: .init(msb: 0x40, lsb: 0x41),
                   data: (msb: 0x10, lsb: 0x00),
                   channel: 0x02)
@@ -436,12 +511,37 @@ final class RPN_NRPN_IO_Tests: XCTestCase {
             0xB2, 0x26, 0x20 // data entry LSB value 0x20
         ])
         
+        // Unit test de-flake:
+        // Filter received events in order to ignore a spurious initial CC message.
+        //
+        // Often systems that run these unit tests are slow or bogged down, so the discrete MIDI 1.0
+        // packets may be fired with slightly too much delay between packets.
+        // Core MIDI allows individual CC 0,6,32,38,98,99,100,101 messages to be received from a
+        // MIDI 1.0 device/endpoint and be delivered to a MIDI 2.0 consumer.
+        // But Core MIDI also has a heuristic which attempts to detect a series of RPN/NRPN CC
+        // messages in discrete packets -- but only if it receives these messages in short enough
+        // succession with nearly no delay between them. Then it will drop the individual CC
+        // messages and produce a single MIDI 2.0 RPN/NRPN UMP packet containing all of the
+        // pertinent bytes.
+        // However, what happens when the MIDI 1.0 packets are sent with too much delay between them
+        // is Core MIDI will pass the first CC message through as-is, assuming that it is a discrete
+        // CC message. but once it sees the second CC message that defines the 2nd message in a
+        // 2-message RPN/NRPN header (CC 101 and CC 100, or CC 99 and CC 98) then it realizes that
+        // this 2nd message and any Data Entry MSB (CC 0) that follows should be all packaged up
+        // into a MIDI 2.0 RPN/NRPN UMP packet.
+        // The result is that we receive either a CC 101 or 99, followed by the expected RPN/NRPN
+        // event.
+        // The solution for de-flaking the unit test is to just ignore all non-RPN/NRPN events.
+        func filteredEvents() -> [MIDIEvent] {
+            receivedEvents.filter(chanVoice: .keepType(.nrpn))
+        }
+        
         // wait(sec: 1.0)
-        wait(for: receivedEvents.count == 2, timeout: 1.0)
+        wait(for: filteredEvents().count == 2, timeout: 1.0)
         
         // Core MIDI translates MIDI 1.0 NRPN to a MIDI 2.0 UMP packet with MSB first,
         // then a second UMP packet adding the LSB to the same base packet data.
-        XCTAssertEqual(receivedEvents, [
+        XCTAssertEqual(filteredEvents(), [
             .nrpn(parameter: .init(msb: 0x40, lsb: 0x41),
                   data: (msb: 0x10, lsb: 0x00),
                   channel: 0x02),
