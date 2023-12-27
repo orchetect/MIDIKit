63d446d25b666ef04f4ec3b2512a68dcf3c4e6a8
Steffan Andrews <stef@orchetect.com>
2023-12-26 16:57:55 -0800

    `MIDIReceiver`: Internal DRY refactoring

--- Sources/MIDIKitIO/MIDIReceiveHandler/Handlers/Events.swift
+++ Sources/MIDIKitIO/MIDIReceiveHandler/Handlers/Events.swift
@@ -21,43 +21,8 @@ extension MIDIReceiveHandler {
     /// MIDI Event receive handler including packet timestamp and source endpoint.
     /// Source endpoint is only available when used with ``MIDIInputConnection`` and will always be
     /// `nil` when used with ``MIDIInput``.
-    final class Events: MIDIReceiveHandlerProtocol {
+    final class Events: EventsBase {
         public var handler: MIDIReceiver.EventsHandler
-        
-        let midi1Parser: MIDI1Parser
-        
-        var midi2Parser: MIDI2Parser? = nil
-        var advancedMIDI2Parser: AdvancedMIDI2Parser? = nil
-        
-        public func packetListReceived(
-            _ packets: [MIDIPacketData]
-        ) {
-            for midiPacket in packets {
-                let events = midi1Parser.parsedEvents(in: midiPacket)
-                guard !events.isEmpty else { continue }
-                
-                handler(events, midiPacket.timeStamp, midiPacket.source)
-            }
-        }
-    
-        @available(macOS 11, iOS 14, macCatalyst 14, *)
-        public func eventListReceived(
-            _ packets: [UniversalMIDIPacketData],
-            protocol midiProtocol: MIDIProtocolVersion
-        ) {
-            if let parser = midi2Parser {
-                for midiPacket in packets {
-                    let events = parser.parsedEvents(in: midiPacket)
-                    guard !events.isEmpty else { continue }
-                    
-                    handler(events, midiPacket.timeStamp, midiPacket.source)
-                }
-            } else if let parser = advancedMIDI2Parser {
-                for midiPacket in packets {
-                    parser.parseEvents(in: midiPacket)
-                }
-            }
-        }
         
         init(
             options: MIDIReceiverOptions,
@@ -73,22 +38,15 @@ extension MIDIReceiveHandler {
                 self.handler = handler
             }
             
-            // MIDI 1
-            
-            midi1Parser = MIDI1Parser()
-            
-            midi1Parser.translateNoteOnZeroVelocityToNoteOff = options
-                .contains(.translateMIDI1NoteOnZeroVelocityToNoteOff)
-            
-            // MIDI 2
-            
-            if options.contains(.bundleRPNAndNRPNDataEntryLSB) {
-                advancedMIDI2Parser = AdvancedMIDI2Parser { [weak self] events, timeStamp, source in
-                    self?.handler(events, timeStamp, source)
-                }
-            } else {
-                midi2Parser = MIDI2Parser()
-            }
+            super.init(options: options)
+        }
+        
+        override func handle(
+            events: [MIDIEvent],
+            timeStamp: CoreMIDITimeStamp,
+            source: MIDIOutputEndpoint?
+        ) {
+            handler(events, timeStamp, source)
         }
     }
 }
--- Sources/MIDIKitIO/MIDIReceiveHandler/Handlers/WeakEventsReceiver.swift
+++ Sources/MIDIKitIO/MIDIReceiveHandler/Handlers/WeakEventsReceiver.swift
@@ -9,70 +9,23 @@
 extension MIDIReceiveHandler {
     /// MIDI Event receive handler that holds a weak reference to a receiver object that conforms
     /// to the ``ReceivesMIDIEvents`` protocol.
-    final class WeakEventsReceiver: MIDIReceiveHandlerProtocol {
+    final class WeakEventsReceiver: EventsBase {
         public weak var receiver: ReceivesMIDIEvents?
-        
-        let midi1Parser: MIDI1Parser
-        
-        var midi2Parser: MIDI2Parser? = nil
-        var advancedMIDI2Parser: AdvancedMIDI2Parser? = nil
-        
-        let options: MIDIReceiverOptions
-        
-        public func packetListReceived(
-            _ packets: [MIDIPacketData]
-        ) {
-            for midiPacket in packets {
-                let events = midi1Parser.parsedEvents(in: midiPacket)
-                guard !events.isEmpty else { continue }
-                handle(events: events)
-            }
-        }
-        
-        @available(macOS 11, iOS 14, macCatalyst 14, *)
-        public func eventListReceived(
-            _ packets: [UniversalMIDIPacketData],
-            protocol midiProtocol: MIDIProtocolVersion
-        ) {
-            if let parser = midi2Parser {
-                for midiPacket in packets {
-                    let events = parser.parsedEvents(in: midiPacket)
-                    guard !events.isEmpty else { continue }
-                    handle(events: events)
-                }
-            } else if let parser = advancedMIDI2Parser {
-                for midiPacket in packets {
-                    parser.parseEvents(in: midiPacket)
-                }
-            }
-        }
         
         init(
             options: MIDIReceiverOptions,
             receiver: ReceivesMIDIEvents
         ) {
-            self.options = options
             self.receiver = receiver
             
-            // MIDI 1
-            
-            midi1Parser = MIDI1Parser()
-            
-            midi1Parser.translateNoteOnZeroVelocityToNoteOff = options
-                .contains(.translateMIDI1NoteOnZeroVelocityToNoteOff)
-            
-            // MIDI 2
-            
-            if options.contains(.bundleRPNAndNRPNDataEntryLSB) {
-                advancedMIDI2Parser = AdvancedMIDI2Parser { [weak self] events, _, _ in
-                    self?.handle(events: events)
-                }
-            } else {
-                midi2Parser = MIDI2Parser()
-            }
+            super.init(options: options)
         }
         
-        func handle(events: [MIDIEvent]) {
+        override func handle(
+            events: [MIDIEvent],
+            timeStamp: CoreMIDITimeStamp,
+            source: MIDIOutputEndpoint?
+        ) {
             if options.contains(.filterActiveSensingAndClock) {
                 let filtered = events.filter(sysRealTime: .dropTypes([.activeSensing, .timingClock]))
                 guard !filtered.isEmpty else { return }
