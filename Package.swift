4e983a76526024d0e1330d027854c90a9aaaf470
Steffan Andrews <stef@orchetect.com>
2024-06-24 21:58:57 -0700

    Bumped TimecodeKit to 2.0.11

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.0.10"),
+        .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.0.11"),
         
         // testing only:
         .package(url: "https://github.com/orchetect/XCTestUtils", from: "1.0.3")
