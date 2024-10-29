f229dbbc70e1335d8e55f1a4e656c5442cf82df3
Steffan Andrews <stef@orchetect.com>
2024-10-28 23:39:28 -0700

    Updated unit tests

--- Tests/MIDIKitSyncTests/Stress Tests/Stress Tests.swift
+++ Tests/MIDIKitSyncTests/Stress Tests/Stress Tests.swift
@@ -9,7 +9,7 @@ import CoreMIDI
 import XCTest
 
 final class StressTests: XCTestCase {
-    func testThreadingMTCGenerator() {
+    func testThreadingMTCGenerator() async {
         // MARK: - Generator
         
         let mtcGen = MTCGenerator { midiMessage in
@@ -19,46 +19,49 @@ final class StressTests: XCTestCase {
         // test public properties and methods
         // to make sure we don't encounter thread-related crashes
         
-        func access() {
+        func access() async {
             // public properties (set and get where applicable)
-            _ = mtcGen.name
-            _ = mtcGen.mtcFrameRate
-            _ = mtcGen.state
-            _ = mtcGen.timecode
-            _ = mtcGen.localFrameRate
-            _ = mtcGen.locateBehavior
-            mtcGen.locateBehavior = .always
-            _ = mtcGen.midiOutHandler
-            mtcGen.midiOutHandler = { _ in }
+            _ = await mtcGen.name
+            _ = await mtcGen.mtcFrameRate
+            _ = await mtcGen.state
+            _ = await mtcGen.timecode
+            _ = await mtcGen.localFrameRate
+            _ = await mtcGen.locateBehavior
+            await mtcGen.setLocateBehavior(.always)
+            _ = await mtcGen.midiOutHandler
+            await mtcGen.setMIDIOutHandler { _ in }
             
             // public methods
-            mtcGen.locate(to: Timecode(.zero, at: .fps24))
-            mtcGen.locate(to: Timecode.Components.zero)
-            mtcGen.start(now: Timecode(.zero, at: .fps24))
-            mtcGen.stop()
-            mtcGen.start(now: Timecode.Components.zero, frameRate: .fps24, base: .max100SubFrames)
-            mtcGen.stop()
-            mtcGen.start(now: 0.0, frameRate: .fps24)
-            mtcGen.stop()
+            await mtcGen.locate(to: Timecode(.zero, at: .fps24))
+            await mtcGen.locate(to: Timecode.Components.zero)
+            await mtcGen.start(now: Timecode(.zero, at: .fps24))
+            await mtcGen.stop()
+            await mtcGen.start(now: Timecode.Components.zero, frameRate: .fps24, base: .max100SubFrames)
+            await mtcGen.stop()
+            await mtcGen.start(now: 0.0, frameRate: .fps24)
+            await mtcGen.stop()
         }
         
         // from same thread as its allocation
-        access()
+        await access()
         
         // from different thread
-        DispatchQueue.global().sync {
-            access()
-        }
+        _ = await Task {
+            await access()
+        }.value
     }
     
-    func testThreadingMTCReceiver() {
+    func testThreadingMTCReceiver() async {
         // MARK: - Receiver
         
         // (Receiver.midiIn() is async internally so we need to wait for
         // property updates to occur before reading them)
         
         // init with local frame rate
-        let mtcRec = MTCReceiver(name: "test", initialLocalFrameRate: .fps24) { timecode, messageType, direction, displayNeedsUpdate in
+        let mtcRec = MTCReceiver(
+            name: "test",
+            initialLocalFrameRate: .fps24
+        ) { timecode, messageType, direction, displayNeedsUpdate in
             _ = timecode
             _ = messageType
             _ = direction
@@ -70,29 +73,29 @@ final class StressTests: XCTestCase {
         // test public properties and methods
         // to make sure we don't encounter thread-related crashes
         
-        func access() {
+        func access() async {
             // public properties (set and get where applicable)
-            _ = mtcRec.state
-            _ = mtcRec.timecode
-            _ = mtcRec.localFrameRate
-            mtcRec.localFrameRate = .fps30
-            _ = mtcRec.mtcFrameRate
-            _ = mtcRec.direction
-            _ = mtcRec.syncPolicy
-            mtcRec.syncPolicy = .init()
-            _ = mtcRec.timecodeChangedHandler
-            mtcRec.timecodeChangedHandler = { _, _, _, _ in }
+            _ = await mtcRec.state
+            _ = await mtcRec.timecode
+            _ = await mtcRec.localFrameRate
+            await mtcRec.setLocalFrameRate(.fps30)
+            _ = await mtcRec.mtcFrameRate
+            _ = await mtcRec.direction
+            _ = await mtcRec.syncPolicy
+            await mtcRec.setSyncPolicy(.init(lockFrames: 0, dropOutFrames: 10))
+            _ = await mtcRec.timecodeChangedHandler
+            await mtcRec.setTimecodeChangedHandler { _, _, _, _ in }
             
             // public methods
             // (none)
         }
         
         // from same thread as its allocation
-        access()
+        await access()
         
         // from different thread
-        DispatchQueue.global().sync {
-            access()
-        }
+        _ = await Task {
+            await access()
+        }.value
     }
 }
