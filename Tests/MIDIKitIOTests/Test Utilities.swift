c080705cf07f6ac7eb14e71f7d46b5ceab40f9b4
Steffan Andrews <stef@orchetect.com>
2025-01-25 15:16:52 -0800

    Added additional unit test utilities, fixed unit test build on tvOS / watchOS

--- Tests/MIDIKitIOTests/Test Utilities.swift
+++ Tests/MIDIKitIOTests/Test Utilities.swift
@@ -7,8 +7,13 @@
 import Foundation
 import Testing
 
-#if !os(watchOS)
+// ⚠️ NOTE:
+//
+// This file is duplicated in the MIDIKitIOTests target.
+// Ensure changes here are also reflected there.
+//
 
+/// Wait for a boolean condition, failing an expectation if the condition times out without evaluating to `true`.
 func wait(
     expect condition: @Sendable () async throws -> Bool,
     timeout: TimeInterval,
@@ -16,18 +21,19 @@ func wait(
     _ comment: Testing.Comment? = nil,
     sourceLocation: Testing.SourceLocation = #_sourceLocation
 ) async rethrows {
-    let pollingIntervalMS = Int(pollingInterval * TimeInterval(MSEC_PER_SEC))
+    let pollingIntervalNS = UInt64(pollingInterval * TimeInterval(NSEC_PER_SEC))
     
     let startTime = Date()
     
     while Date().timeIntervalSince(startTime) < timeout {
         if try await condition() { return }
-        try? await Task.sleep(for: .milliseconds(pollingIntervalMS))
+        try? await Task.sleep(nanoseconds: pollingIntervalNS)
     }
     
     #expect(try await condition(), comment, sourceLocation: sourceLocation)
 }
 
+/// Wait for a boolean condition, throwing an error if the condition times out without evaluating to `true`.
 func wait(
     require condition: @Sendable () async throws -> Bool,
     timeout: TimeInterval,
@@ -35,21 +41,25 @@ func wait(
     _ comment: Testing.Comment? = nil,
     sourceLocation: Testing.SourceLocation = #_sourceLocation
 ) async throws {
-    let pollingIntervalMS = Int(pollingInterval * TimeInterval(MSEC_PER_SEC))
+    let pollingIntervalNS = UInt64(pollingInterval * TimeInterval(NSEC_PER_SEC))
     
     let startTime = Date()
     
     while Date().timeIntervalSince(startTime) < timeout {
         if try await condition() { return }
-        try await Task.sleep(for: .milliseconds(pollingIntervalMS))
+        try await Task.sleep(nanoseconds: pollingIntervalNS)
     }
     
     try #require(await condition(), comment, sourceLocation: sourceLocation)
 }
 
-#endif
-
 /// Use as a condition for individual tests that rely on stable/precise system timing.
+///
+/// For example:
+///
+/// ```swift
+/// @Test(.enabled(if: isSystemTimingStable())) func foo { }
+/// ```
 func isSystemTimingStable(
     duration: TimeInterval = 0.1,
     tolerance: TimeInterval = 0.01
@@ -63,4 +73,19 @@ func isSystemTimingStable(
     
     let range = (duration - tolerance) ... (duration + tolerance)
     return range.contains(diff)
+}
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+extension Task where Success == Never, Failure == Never {
+    /// Suspends the current task for at least the given duration
+    /// in seconds.
+    ///
+    /// If the task is canceled before the time ends,
+    /// this function throws `CancellationError`.
+    ///
+    /// This function doesn't block the underlying thread.
+    static func sleep(seconds: TimeInterval) async throws {
+        let intervalNS = UInt64(seconds * TimeInterval(NSEC_PER_SEC))
+        try await sleep(nanoseconds: intervalNS)
+    }
 }
--- Tests/MIDIKitSyncTests/MTC/Receiver/MTC Receiver Tests.swift
+++ Tests/MIDIKitSyncTests/MTC/Receiver/MTC Receiver Tests.swift
@@ -62,7 +62,7 @@ import Testing
         
         // 01:02:03:04 @ MTC 24fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._01_02_03_04_at_24fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         await #expect(
             mtcRec.timecode ==
             Timecode(.components(h: 1, m: 2, s: 3, f: 4), at: .fps24, by: .allowingInvalid)
@@ -70,7 +70,7 @@ import Testing
         
         // 00:00:00:00 @ MTC 24fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._00_00_00_00_at_24fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         await #expect(
             mtcRec.timecode ==
             Timecode(.components(h: 0, m: 0, s: 0, f: 0), at: .fps24, by: .allowingInvalid)
@@ -78,7 +78,7 @@ import Testing
         
         // 02:11:17:20 @ MTC 25fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._02_11_17_20_at_25fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         await #expect(
             mtcRec.timecode ==
             Timecode(.components(h: 2, m: 11, s: 17, f: 20), at: .fps25, by: .allowingInvalid)
@@ -97,7 +97,7 @@ import Testing
         
         // 01:02:03:04 @ MTC 24fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._01_02_03_04_at_24fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         await #expect(
             mtcRec.timecode ==
             Timecode(.components(h: 1, m: 2, s: 3, f: 4), at: .fps24, by: .allowingInvalid)
@@ -105,7 +105,7 @@ import Testing
         
         // 00:00:00:00 @ MTC 24fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._00_00_00_00_at_24fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         await #expect(
             mtcRec.timecode ==
             Timecode(.components(h: 0, m: 0, s: 0, f: 0), at: .fps24, by: .allowingInvalid)
@@ -113,7 +113,7 @@ import Testing
         
         // 02:11:17:20 @ MTC 25fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._02_11_17_20_at_25fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         await #expect(
             mtcRec.timecode ==
             Timecode(.components(h: 2, m: 11, s: 17, f: 20), at: .fps25, by: .allowingInvalid)
@@ -123,7 +123,7 @@ import Testing
         
         // 02:11:17:20 @ MTC 25fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._02_11_17_20_at_25fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         await #expect(
             mtcRec.timecode ==
             Timecode(.components(h: 2, m: 11, s: 17, f: 20), at: .fps25, by: .allowingInvalid)
@@ -143,7 +143,7 @@ import Testing
         
         // 01:02:03:04 @ MTC 24fps
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._01_02_03_04_at_24fps)
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         
         // state should not change to .incompatibleFrameRate for full frame messages, only quarter
         // frames
@@ -194,26 +194,25 @@ extension MTC_Receiver_Receiver_Tests {
         // 24fps QFs starting at 02:03:04:04, locking at 02:03:04:06 + 2 MTC frame offset
         
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b00000110)) // QF 0
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b00010000)) // QF 1
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b00100100)) // QF 2
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b00110000)) // QF 3
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b01000011)) // QF 4
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b01010000)) // QF 5
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b01100010)) // QF 6
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b01110000)) // QF 7
-        try await Task.sleep(for: .microseconds(010_300)) // approx time between QFs @ 24fps
+        try await Task.sleep(seconds: 0.0103) // approx time between QFs @ 24fps
         await mtcRec.midiIn(event: .timecodeQuarterFrame(dataByte: 0b00001000)) // QF 0
         
-        let waitTimeMS = 5
-        let waitTimeS = TimeInterval(waitTimeMS / 1000)
-        try await Task.sleep(for: .milliseconds(waitTimeMS))
+        let waitTime: TimeInterval = 0.005
+        try await Task.sleep(seconds: waitTime)
         
         await #expect(
             mtcRec.timecode ==
@@ -249,7 +248,7 @@ extension MTC_Receiver_Receiver_Tests {
         // brittle/restrictive and the accuracy may need to be bumped up at some point in the
         // future
         
-        let lhs = (Double(preSyncLockTime.rawValue) / 10e8) + waitTimeS
+        let lhs = (Double(preSyncLockTime.rawValue) / 10e8) + waitTime
         let rhs = Double(futureTime.rawValue) / 10e8
         
         // XCTAssertEqual(lhs, rhs, accuracy: 0.005)
@@ -311,7 +310,7 @@ extension MTC_Receiver_Receiver_Tests {
         
         await mtcRec.midiIn(event: kMIDIEvent.MTC_FullFrame._01_02_03_04_at_24fps)
         
-        try await Task.sleep(for: .milliseconds(50))
+        try await Task.sleep(seconds: 0.050)
         
         #expect(receiver.timecode == Timecode(.components(h: 1, m: 2, s: 3, f: 4), at: .fps24, by: .allowingInvalid))
         #expect(receiver.mType == .fullFrame)
