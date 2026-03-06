3f59672ae2e81ed81baceec93d4b04879653cac0
Steffan Andrews <stef@orchetect.com>
2026-03-06 13:26:29 -0800

    MIDIKitSMF: `MIDIFile`: Refactored loading/decoding raw file contents to parse out chunk ranges before parsing their contents
    This lays the groundwork to allow parsing tracks in parallel with
    concurrency

--- Sources/MIDIKitSMF/MIDIFile/MIDIFile decode.swift
+++ Sources/MIDIKitSMF/MIDIFile/MIDIFile decode.swift
@@ -44,11 +44,15 @@ extension MIDIFile {
             var tracksEncountered = 0
             var endOfFile = false
 
+            var chunkRefs: [(chunkType: String, data: UnsafeBufferPointer<UInt8>, fileByteOffset: Int)] = []
             var newChunks: [Chunk] = []
-
+            
+            // gather chunk references before parsing their contents
+            
             while !endOfFile {
                 // chunk header
 
+                let chunkStartByteOffset = parser.readOffset
                 guard let chunkType = try? parser.read(bytes: 4) else {
                     throw .malformed(
                         "There was a problem reading chunk header at byte offset \(parser.readOffset). Encountered end of file early."
@@ -64,52 +68,58 @@ extension MIDIFile {
                 }
 
                 let chunkTypeString = chunkType.asciiDataToString() ?? "????"
-            
-                try autoreleasepool { () throws(DecodeError) -> Void in
-                    let newChunk: Chunk
-                    
-                    // chunk length
-                    
-                    guard let chunkData = try? parser.read(bytes: Int(chunkLength)) else {
-                        throw .malformed(
-                            "There was a problem reading track data blob at byte offset \(parser.readOffset) for track \(tracksEncountered). Encountered end of file early."
-                        )
-                    }
-                    
-                    do throws(MIDIFile.DecodeError) {
-                        switch chunkTypeString {
-                        case MIDIFile.Chunk.Track.staticIdentifier:
-                            tracksEncountered += 1
-                            
-                            let newTrack = try Chunk.Track(midi1SMFRawBytes: chunkData)
-                            newChunk = .track(newTrack)
-                            
-                        default:
-                            // as per Standard MIDI File 1.0 Spec:
-                            // unrecognized chunks should be skipped and not throw an error
-                            
-                            let newUnrecognizedChunk = Chunk.UnrecognizedChunk(id: chunkTypeString, rawData: chunkData.toData())
-                            newChunk = .other(newUnrecognizedChunk)
-                        }
-                    } catch {
-                        // append some context for the error and rethrow it
-                        switch error {
-                        case let .malformed(verboseError):
-                            throw .malformed(
-                                "There was a problem reading track data at byte offset \(parser.readOffset) for track \(tracksEncountered). \(verboseError)"
-                            )
-                            
-                        default:
-                            throw error
-                        }
-                    }
-                    
-                    newChunks.append(newChunk)
+                
+                // chunk length
+                
+                guard let chunkData: UnsafeBufferPointer<UInt8> = try? parser.read(bytes: Int(chunkLength)) else {
+                    throw .malformed(
+                        "There was a problem reading track data blob at byte offset \(parser.readOffset) for track \(tracksEncountered). Encountered end of file early."
+                    )
                 }
+                
+                chunkRefs.append(
+                    (chunkType: chunkTypeString, data: chunkData, fileByteOffset: chunkStartByteOffset)
+                )
                 
                 if parser.readOffset >= data.count {
                     endOfFile = true
                 }
+            }
+            
+            // parse contents of each chunk
+            
+            for (chunkTypeString, chunkData, fileByteOffset) in chunkRefs {
+                let newChunk: Chunk
+                
+                do throws(MIDIFile.DecodeError) {
+                    switch chunkTypeString {
+                    case MIDIFile.Chunk.Track.staticIdentifier:
+                        tracksEncountered += 1
+                        
+                        let newTrack = try Chunk.Track(midi1SMFRawBytes: chunkData)
+                        newChunk = .track(newTrack)
+                        
+                    default:
+                        // as per Standard MIDI File 1.0 Spec:
+                        // unrecognized chunks should be skipped and not throw an error
+                        
+                        let newUnrecognizedChunk = Chunk.UnrecognizedChunk(id: chunkTypeString, rawData: chunkData.toData())
+                        newChunk = .other(newUnrecognizedChunk)
+                    }
+                } catch {
+                    // append some context for the error and rethrow it
+                    switch error {
+                    case let .malformed(verboseError):
+                        throw .malformed(
+                            "There was a problem reading track data at byte offset \(fileByteOffset) for track \(tracksEncountered). \(verboseError)"
+                        )
+                        
+                    default:
+                        throw error
+                    }
+                }
+                
+                newChunks.append(newChunk)
             }
 
             chunks = newChunks
