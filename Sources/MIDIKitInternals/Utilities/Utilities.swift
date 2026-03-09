bd3f49f415e4347ad2ecba5eb9aa783172acb74b
Steffan Andrews <stef@orchetect.com>
2026-03-09 15:29:44 -0700

    Fixed build in Xcode 16

--- Sources/MIDIKitInternals/Utilities/Utilities.swift
+++ Sources/MIDIKitInternals/Utilities/Utilities.swift
@@ -8,17 +8,20 @@
 
 import Foundation
 
-/// Backwards-compatible implementation of standard lib method for Xcode 16.
-@available(macOS 10.0, iOS 1.0, tvOS 1.0, watchOS 1.0, *)
-package func _autoreleasepool<E, Result>(invoking body: () throws(E) -> Result) throws(E) -> Result where E: Error, Result: ~Copyable {
-    let result: Swift.Result<Result, E> = autoreleasepool {
-        do throws(E) {
-            return .success(try body())
-        } catch {
-            return .failure(error)
-        }
-    }
-    return try result.get()
-}
+// TODO: This was used in MIDIKitSMF prior to MIDIKit 0.12.0 to avoid large memory usage in the MIDIFile decode procedure, but after the decoding refactor this is not being used and may not be needed
+
+// /// Backwards-compatible implementation of standard lib method for Xcode 16.
+// @available(macOS 10.0, iOS 1.0, tvOS 1.0, watchOS 1.0, *)
+// @inlinable
+// package func autoreleasepool<E, Result>(invoking body: () throws(E) -> Result) throws(E) -> Result where E: Error /* , Result: ~Copyable */ {
+//     let result: Swift.Result<Result, E> = Foundation.autoreleasepool {
+//         do throws(E) {
+//             return .success(try body())
+//         } catch {
+//             return .failure(error)
+//         }
+//     }
+//     return try result.get()
+// }
 
 #endif
