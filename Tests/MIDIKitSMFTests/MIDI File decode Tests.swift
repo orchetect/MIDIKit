3a079a4a0b406d979b6e0003aa180e8b42b5a8f0
Steffan Andrews <stef@orchetect.com>
2026-03-09 20:18:40 -0700

    MIDIKitSMF: Updated unit tests

--- Tests/MIDIKitSMFTests/MIDI File decode Tests.swift
+++ Tests/MIDIKitSMFTests/MIDI File decode Tests.swift
@@ -17,7 +17,7 @@ import Testing
     /// Test parsing using NON-ASYNC method.
     /// Just ensure file parses successfully. Not testing contents in-depth.
     @Test
-    func midiFileParse_NonAsync() throws { // DO NOT MARK THIS METHOD ASYNC!!!
+    func midiFileParse_NonAsync() /* NOT ASYNC! */ throws {
         // Note: It's ok if this throws a deprecation warning. We need to test this specific method.
         let midiFile = try /* NOT AWAIT! */ MIDIFile(rawData: kMIDIFile.dp8Markers)
         
@@ -75,13 +75,91 @@ import Testing
         )
     }
     
+    // MARK: - DecodeOptions & Predicate
+    
     @Test
-    func predicate() async throws {
-        let midiFile = try await MIDIFile(rawData: kMIDIFile.dp8Markers)
-        
+    func decodeOptions_maxTrackEventCount() /* NOT ASYNC! */ throws {
+        let options = MIDIFile.DecodeOptions(maxTrackEventCount: 1)
+        // Note: It's ok if this throws a deprecation warning. We need to test this specific method.
+        let midiFile = try /* NOT AWAIT! */ MIDIFile(rawData: kMIDIFile.dp8Markers, options: options)
         try #require(midiFile.chunks.count == 3)
+        try #require(midiFile.tracks.count == 3)
+        for track in midiFile.tracks {
+            #expect(track.events.count == 1)
+        }
+    }
+    
+    @Test
+    func decodeOptions_maxTrackEventCount_async() async throws {
+        let options = MIDIFile.DecodeOptions(
+            maxTrackEventCount: 1
+        )
+        let midiFile = try await MIDIFile(rawData: kMIDIFile.dp8Markers, options: options)
+        try #require(midiFile.chunks.count == 3)
+        try #require(midiFile.tracks.count == 3)
+        for track in midiFile.tracks {
+            #expect(track.events.count == 1)
+        }
+    }
+    
+    @Test
+    func predicate() /* NOT ASYNC! */ throws {
+        // Note: It's ok if this throws a deprecation warning. We need to test this specific method.
+        let midiFile = try /* NOT AWAIT! */ MIDIFile(rawData: kMIDIFile.dp8Markers) { chunkType, chunkIndex in
+            chunkType == .track && chunkIndex == 1
+        }
+        try #require(midiFile.chunks.count == 1)
+        let track = midiFile.tracks[0]
+        #expect(track.events.count == 3) // check if the correct track was included
+    }
+    
+    @Test
+    func predicate_async() async throws {
+        let midiFile = try await MIDIFile(rawData: kMIDIFile.dp8Markers) { chunkType, chunkIndex in
+            chunkType == .track && chunkIndex == 1
+        }
+        try #require(midiFile.chunks.count == 1)
+        let track = midiFile.tracks[0]
+        #expect(track.events.count == 3) // check if the correct track was included
+    }
+    
+    @Test
+    func decodeOptions_bundleRPNAndNRPNEvents() async throws {
+        let rpnEvent = MIDIEvent.RPN(
+            .raw(parameter: .init(msb: 0x05, lsb: 0x10), dataEntryMSB: 0x08, dataEntryLSB: 0x07),
+            channel: 2
+        )
+        let event: MIDIFileEvent = .rpn(delta: .none, event: rpnEvent)
+        let events: [MIDIFileEvent] = [event]
         
-        let encodedData = try midiFile.rawData()
-        #expect(encodedData.toUInt8Bytes() == kMIDIFile.dp8Markers)
+        let track = MIDIFile.Chunk.Track(events: events)
+        let midiFile = MIDIFile(chunks: [.track(track)])
+        
+        let rawData = try midiFile.rawData()
+        
+        // decode without bundleRPNAndNRPNEvents
+        do {
+            let options = MIDIFile.DecodeOptions(bundleRPNAndNRPNEvents: false)
+            let decodedMIDIFile = try await MIDIFile(rawData: rawData, options: options)
+            #expect(decodedMIDIFile.tracks.count == 1)
+            let decodedTrack = decodedMIDIFile.tracks[0]
+            #expect(decodedTrack.events.count == 4)
+            #expect(decodedTrack.events == [
+                .cc(delta: .none, event: .init(controller: .rpnMSB, value: .midi1(0x05), channel: 2)),
+                .cc(delta: .none, event: .init(controller: .rpnLSB, value: .midi1(0x10), channel: 2)),
+                .cc(delta: .none, event: .init(controller: .dataEntry, value: .midi1(0x08), channel: 2)),
+                .cc(delta: .none, event: .init(controller: .lsb(for: .dataEntry), value: .midi1(0x07), channel: 2))
+            ])
+        }
+        
+        // decode with bundleRPNAndNRPNEvents
+        do {
+            let options = MIDIFile.DecodeOptions(bundleRPNAndNRPNEvents: true)
+            let decodedMIDIFile = try await MIDIFile(rawData: rawData, options: options)
+            #expect(decodedMIDIFile.tracks.count == 1)
+            let decodedTrack = decodedMIDIFile.tracks[0]
+            #expect(decodedTrack.events.count == 1)
+            #expect(decodedTrack.events == events)
+        }
     }
 }
