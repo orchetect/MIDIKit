a0ef6dbce5a9f0ce6e917b19542a36afe1eb26c5
Steffan Andrews <stef@orchetect.com>
2026-03-11 14:42:19 -0700

    MIDIKitSMF: `MIDIFile`: `DecodeOptions`: Added `ignoreBytesPastEOF` property (#177)

--- Sources/MIDIKitSMF/MIDIFile/MIDIFile DecodeOptions.swift
+++ Sources/MIDIKitSMF/MIDIFile/MIDIFile DecodeOptions.swift
@@ -7,15 +7,36 @@
 extension MIDIFile {
     /// MIDI file decoding options.
     public struct DecodeOptions {
+        /// Bundle RPN/NRPN CC message sequences into `rpn`/`nrpn` event types.
+        /// If `false`, the message sequences will be parsed as individual CC messages.
         public var bundleRPNAndNRPNEvents: Bool
+        
+        /// The maximum number of events parsed from each track.
+        /// If `nil`, all events are parsed.
         public var maxTrackEventCount: Int?
+        
+        /// Ignore any bytes that may be present past the "end of file".
+        /// If `false`, if any extraneous bytes are present it is considered a malformed file and an error is thrown.
+        ///
+        /// As long the parser has enough bytes to parse the entire body of the file data and it parses without
+        /// issue, we can safely ignore any bytes that follow - regardless of what they are (CR, LF, whitespace, or
+        /// other random bytes).
+        ///
+        /// Some MIDI files may have trailing CR/LF bytes or whitespace, either from poorly implemented MIDI file encoders,
+        /// or if they were opened in a text editor and resaved by an individual. This parsing option is a
+        /// mitigation to work around these type of scenarios.
+        ///
+        /// See issue for more details: https://github.com/orchetect/MIDIKit/issues/177
+        public var ignoreBytesPastEOF: Bool
         
         public init(
             bundleRPNAndNRPNEvents: Bool = true,
-            maxTrackEventCount: Int? = nil
+            maxTrackEventCount: Int? = nil,
+            ignoreBytesPastEOF: Bool = true
         ) {
             self.bundleRPNAndNRPNEvents = bundleRPNAndNRPNEvents
             self.maxTrackEventCount = maxTrackEventCount
+            self.ignoreBytesPastEOF = ignoreBytesPastEOF
         }
     }
 }
