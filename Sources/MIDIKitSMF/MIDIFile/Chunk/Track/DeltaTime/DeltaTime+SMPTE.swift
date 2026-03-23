4a50c59c903b98731efafce11d93a5e7ad0c5690
Steffan Andrews <stef@orchetect.com>
2026-03-23 00:56:34 -0700

    MIDIKitSMF: `MIDIFile.TrackChunk.DeltaTime`: Added static constructors for SMPTE timebase

--- Sources/MIDIKitSMF/MIDIFile/Chunk/Track/DeltaTime/DeltaTime+SMPTE.swift
+++ Sources/MIDIKitSMF/MIDIFile/Chunk/Track/DeltaTime/DeltaTime+SMPTE.swift
@@ -7,5 +7,33 @@
 // MARK: - Static Constructors
 
 extension MIDIFile.TrackChunk.DeltaTime where Timebase == SMPTEMIDIFileTimebase {
-    // TODO: add constructors that make sense for timecode/frames
+    /// Construct delta time duration from SMPTE timecode.
+    /// The `offset` frame rate and `ticksPerFrame` should match the values specified in the MIDI file header.
+    public static func offset(_ offset: MIDIFileTrackEvent.SMPTEOffset, ticksPerFrame: UInt8) -> Self {
+        let frameCount = offset.timecode.frameCount
+        let ticks = frameCount.doubleValue * Double(ticksPerFrame)
+        return Self(ticks: UInt32(ticks))
+    }
+    
+    /// Construct delta time duration from SMPTE timecode.
+    /// The `offset` frame rate and `ticksPerFrame` should match the values specified in the MIDI file header.
+    public static func offset(
+        hr: UInt8,
+        min: UInt8,
+        sec: UInt8,
+        fr: UInt8,
+        subFr: UInt8 = 0,
+        frRate: MIDIFileFrameRate,
+        ticksPerFrame: UInt8
+    ) -> Self {
+        let smpteOffset = MIDIFileTrackEvent.SMPTEOffset(
+            hr: hr,
+            min: min,
+            sec: sec,
+            fr: fr,
+            subFr: subFr,
+            frRate: frRate
+        )
+        return .offset(smpteOffset, ticksPerFrame: ticksPerFrame)
+    }
 }
