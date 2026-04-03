776089f2790e9a61f354d67f4d2bf6bb613922ee
Steffan Andrews <stef@orchetect.com>
2026-04-03 15:04:16 -0700

    MIDIKitSMF: Added placeholder files for MIDI Clip File and MIDI Container File (#274)

---
+++ Sources/MIDIKitSMF/MIDI2File/MIDI2ContainerFile.swift
@@ -0,0 +1,10 @@
+//
+//  MIDI2ContainerFile.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2025 Steffan Andrews • Licensed under MIT License
+//
+
+// TODO: Implement MIDI Container File (SMF 2.0)
+// See: https://github.com/orchetect/MIDIKit/issues/274
+
+// public struct MIDI2ContainerFile { }
