700f6dc5bff4147de89aabe0ffb192cc46d87929
Steffan Andrews <stef@orchetect.com>
2026-04-24 14:48:14 -0700

    Added SwiftMIDI target

---
+++ Sources/SwiftMIDI/SwiftMIDI.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftMIDI.swift
+//  swift-midi • https://github.com/orchetect/swift-midi
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+// Welcome to swift-midi :)
+
+// MARK: SwiftMIDICore
+
+@_documentation(visibility: internal)
+@_exported public import SwiftMIDICore
+
+// MARK: SwiftMIDIIO
+
+@_documentation(visibility: internal)
+@_exported public import SwiftMIDIIO
+
+// MARK: SwiftMIDIControlSurfaces
+
+@_documentation(visibility: internal)
+@_exported public import SwiftMIDIControlSurfaces
+
+// MARK: SwiftMIDIFile
+
+@_documentation(visibility: internal)
+@_exported public import SwiftMIDIFile
+
+@_documentation(visibility: internal)
+@_exported public import SwiftTimecodeCore // must re-export target's exports here
+
+// MARK: SwiftMIDISync
+
+@_documentation(visibility: internal)
+@_exported public import SwiftMIDISync
+
+// MARK: SwiftMIDIUI
+
+@_documentation(visibility: internal)
+@_exported public import SwiftMIDIUI
