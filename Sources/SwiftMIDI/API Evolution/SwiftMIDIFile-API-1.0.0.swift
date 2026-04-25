b3c7e130c10d49d54af231b78411286682b21b4c
Steffan Andrews <stef@orchetect.com>
2026-04-25 12:55:50 -0700

    Added SwiftMIDIFile API evolution

---
+++ Sources/SwiftMIDI/API Evolution/SwiftMIDIFile-API-1.0.0.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftMIDIFile-API-1.0.0.swift
+//  swift-midi • https://github.com/orchetect/swift-midi
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftMIDIFile
+
+@_documentation(visibility: internal)
+@available(*, deprecated, renamed: "MusicalMIDI1File")
+public typealias MIDIFile = MusicalMIDI1File
